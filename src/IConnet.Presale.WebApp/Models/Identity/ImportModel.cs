22542b86453af1904ce9404954e6b90de1809c8e
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-18 13:04:18 +0700

    add 'Import' model

---
+++ src/IConnet.Presale.WebApp/Models/Identity/ImportModel.cs
@@ -0,0 +1,36 @@
+#nullable disable
+using IConnet.Presale.Shared.Interfaces.Models.Presales;
+
+namespace IConnet.Presale.WebApp.Models.Identity;
+
+public class ImportModel : IApprovalOpportunityModel
+{
+    public string IdPermohonan { get; set; }
+    public string TglPermohonan { get; set; }
+    public string DurasiTidakLanjut { get; set; }
+    public string NamPemohon { get; set; }
+    public string IdPln { get; set; }
+    public string Layanan { get; set; }
+    public string SumberPemohon { get; set; }
+    public string StatusPermohonan { get; set; }
+    public string NamaAgen { get; set; }
+    public string EmailAgen { get; set; }
+    public string TeleponAgen { get; set; }
+    public string MitraAgen { get; set; }
+    public string Splitter { get; set; }
+    public string JenisPermohonan { get; set; }
+    public string TeleponPemohon { get; set; }
+    public string EmailPemohon { get; set; }
+    public string NikPemohon { get; set; }
+    public string NpwpPemohon { get; set; }
+    public string Keterangan { get; set; }
+    public string AlamatPemohon { get; set; }
+    public string Regional { get; set; }
+    public string KantorPerwakilan { get; set; }
+    public string Provinsi { get; set; }
+    public string Kabupaten { get; set; }
+    public string Kecamatan { get; set; }
+    public string Kelurahan { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
+}
