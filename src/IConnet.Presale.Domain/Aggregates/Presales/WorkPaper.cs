128789f5f467512b3f93e7715c27528de3c8ac5b
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-19 01:59:35 +0700

    add 'WorkPaper'

---
+++ src/IConnet.Presale.Domain/Aggregates/Presales/WorkPaper.cs
@@ -0,0 +1,39 @@
+#nullable disable
+
+namespace IConnet.Presale.Domain.Aggregates.Presales;
+
+public class WorkPaper : IAggregateRoot
+{
+    public WorkPaper()
+    {
+        ApprovalOpportunity = new ApprovalOpportunity();
+        ValidationParameter = new ValidationParameter();
+    }
+
+    public Guid WorkPaperId { get; set; }
+    public ApprovalOpportunity ApprovalOpportunity { get; set; }
+    public string HelpdeskClaim { get; set; }
+    public string HelpdeskShift { get; set; }
+    public DateTimeOffset ChatCallDateTime { get; set; }
+    public DateTimeOffset ResponseDateTime { get; set; }
+    public string ChatHistoryRecapLink { get; set; }
+    public ValidationParameter ValidationParameter { get; set; }
+    public string ValidationStatus { get; set; }
+    public string HelpdeskNote { get; set; }
+    public string ApprovalStatus { get; set; }
+    public string PacClaim { get; set; }
+    public string ApprovalNote { get; set; }
+    public string ShareLocDistance { get; set; }
+    public string ICrmPlusDistance { get; set; }
+    public DateTime VirtualAccountPublish { get; set; }
+}
+
+public class ValidationParameter
+{
+    public string PhoneNumber { get; set; }
+    public string CustomerName { get; set; }
+    public string EmailAddress { get; set; }
+    public string CustomerAddress { get; set; }
+    public string PlnId { get; set; }
+    public string ShareLoc { get; set; }
+}
