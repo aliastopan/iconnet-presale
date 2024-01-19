39c7bb30b70bce316e08b6467ea81746c34c6ab3
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-19 17:48:52 +0700

    chore: arrange props priority

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValidationParameter.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValidationParameter.cs
@@ -4,10 +4,10 @@ namespace IConnet.Presale.Domain.Aggregates.Presales;
 
 public class ValidationParameter
 {
-    public string NomorTelepon { get; set; }
+    public string PlnId { get; set; }
     public string NamaPelanggan { get; set; }
+    public string NomorTelepon { get; set; }
     public string Email { get; set; }
     public string AlamatPelanggan { get; set; }
-    public string PlnId { get; set; }
     public string ShareLoc { get; set; }
 }
