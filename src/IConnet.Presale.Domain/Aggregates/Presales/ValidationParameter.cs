bfc211c76925f4a06453c963d5d01c9bf55ccaea
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-19 17:51:00 +0700

    refactor: 'Coordinate' type 'ShareLoc'

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValidationParameter.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValidationParameter.cs
@@ -4,10 +4,15 @@ namespace IConnet.Presale.Domain.Aggregates.Presales;
 
 public class ValidationParameter
 {
+    public ValidationParameter()
+    {
+        ShareLoc = new Coordinate();
+    }
+
     public string PlnId { get; set; }
     public string NamaPelanggan { get; set; }
     public string NomorTelepon { get; set; }
     public string Email { get; set; }
     public string AlamatPelanggan { get; set; }
-    public string ShareLoc { get; set; }
+    public Coordinate ShareLoc { get; set; }
 }
