6e8aba3758f5d5d576fce130b7a9d9efc57b9bb0
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-19 14:34:53 +0700

    'Import' model mapping

--- src/IConnet.Presale.Infrastructure/Managers/WorkloadManager.cs
+++ src/IConnet.Presale.Infrastructure/Managers/WorkloadManager.cs
@@ -14,6 +14,51 @@ internal sealed class WorkloadManager : IWorkloadManager
 
     public Task CreateWorkPaper(IApprovalOpportunityModel ImportModel)
     {
+        var ApprovalOpportunity = new ApprovalOpportunity
+        {
+            ApplicationId = ImportModel.IdPermohonan,
+            ApplicationDate = _dateTimeService.ParseExact(ImportModel.TglPermohonan),
+            ApplicationSource = ImportModel.SumberPermohonan,
+            ApplicationStatus = ImportModel.StatusPermohonan,
+            ApplicationType = ImportModel.JenisPermohonan,
+            Service = ImportModel.Layanan,
+            Splitter = ImportModel.Splitter,
+            Applicant = new Applicant
+            {
+                FullName = ImportModel.NamaPemohon,
+                PlnId = ImportModel.IdPln,
+                EmailAddress = ImportModel.EmailPemohon,
+                PhoneNumber = ImportModel.TeleponPemohon,
+                Nik = ImportModel.NikPemohon,
+                Npwp = ImportModel.NpwpPemohon,
+                Information = ImportModel.Keterangan,
+                Address = ImportModel.AlamatPemohon
+            },
+            Agent = new Agent
+            {
+                FullName = ImportModel.NamaAgen,
+                EmailAddress = ImportModel.EmailAgen,
+                PhoneNumber = ImportModel.TeleponAgen,
+                Partner = ImportModel.MitraAgen
+            },
+            Regional = new Regional
+            {
+                Bagian = ImportModel.Regional,
+                RepresentativeOffice = ImportModel.KantorPerwakilan,
+                Provinsi = ImportModel.Provinsi,
+                Kabupaten = ImportModel.Kabupaten,
+                Kecamatan = ImportModel.Kecamatan,
+                Kelurahan = ImportModel.Kelurahan,
+                Coordinate = new Coordinate
+                {
+                    Latitude = ImportModel.Latitude,
+                    Longitude = ImportModel.Longitude
+                }
+            },
+            ImportDateTime = ImportModel.ImportDateTime.DateTime,
+            ImportClaimName = ImportModel.ImportClaimName
+        };
+
        return Task.CompletedTask;
     }
 }
