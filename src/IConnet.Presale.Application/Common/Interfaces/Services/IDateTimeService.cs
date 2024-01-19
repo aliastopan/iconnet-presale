e492d3e94d3ce3356472a1718de613c8c2d5bfa1
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-19 14:13:28 +0700

    add 'DateTime' format

--- src/IConnet.Presale.Application/Common/Interfaces/Services/IDateTimeService.cs
+++ src/IConnet.Presale.Application/Common/Interfaces/Services/IDateTimeService.cs
@@ -4,4 +4,5 @@ public interface IDateTimeService
 {
     DateTime UtcNow { get; }
     DateTimeOffset DateTimeOffsetNow { get; }
+    String Format { get; }
 }
--- src/IConnet.Presale.Infrastructure/Services/DateTimeProvider.cs
+++ src/IConnet.Presale.Infrastructure/Services/DateTimeProvider.cs
@@ -4,4 +4,5 @@ internal sealed class DateTimeProvider : IDateTimeService
 {
     public DateTime UtcNow => DateTime.UtcNow;
     public DateTimeOffset DateTimeOffsetNow => DateTimeOffset.Now;
+    public string Format => "yyyy-MM-dd HH:mm";
 }
