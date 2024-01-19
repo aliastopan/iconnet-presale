594e96759431919b1f7449eafc12ea2dec6e16d5
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-19 20:28:48 +0700

    add global usings

--- src/IConnet.Presale.WebApp/Usings.cs
+++ src/IConnet.Presale.WebApp/Usings.cs
@@ -1,2 +1,5 @@
+global using IConnet.Presale.Application.Common.Interfaces.Managers;
+global using IConnet.Presale.Application.Common.Interfaces.Services;
+
 global using AnnotatedResult;
 global using Serilog;
