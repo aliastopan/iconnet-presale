537738b4a27fafc5eef91e5606be063f88c07203
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-20 17:47:30 +0700

    add instant property

--- src/IConnet.Presale.Domain/Aggregates/Presales/Coordinate.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/Coordinate.cs
@@ -1,4 +1,5 @@
 #nullable disable
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IConnet.Presale.Domain.Aggregates.Presales;
 
@@ -6,4 +7,7 @@ public class Coordinate
 {
     public string Latitude { get; set; }
     public string Longitude { get; set; }
+
+    [NotMapped]
+    public string LatitudeLongitude => $"{Latitude}, {Longitude}";
 }
