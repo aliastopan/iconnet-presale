8bc61f830f8761899e04a1adbbcd26936c7d91e1
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-20 19:56:02 +0700

    seed more mock users

--- src/IConnet.Presale.Infrastructure/Services/DataSeedingProvider.cs
+++ src/IConnet.Presale.Infrastructure/Services/DataSeedingProvider.cs
@@ -53,9 +53,66 @@ internal sealed class DataSeedingProvider : IDataSeedingService
             LastSignedIn = _dateTimeService.DateTimeOffsetNow
         };
 
+        var helpdesk01 = new UserAccount
+        {
+            UserAccountId = Guid.Parse("73946fa9-d92d-453a-a5c4-1f3daea5d66f"),
+            User = new User
+            {
+                Username = "helpdesk",
+                EmailAddress = "helpdesk.first@mail.me",
+                UserRole = UserRole.Helpdesk,
+                UserPrivileges = new List<UserPrivilege>()
+                {
+                    UserPrivilege.Viewer,
+                    UserPrivilege.Editor,
+                },
+                UserShift = UserShift.Siang
+            },
+            UserProfile = new UserProfile
+            {
+                FirstName = "Andy",
+                LastName = "Wijaya",
+                DateOfBirth = new DateOnly(year: 1997, month: 5, day: 10)
+            },
+            PasswordHash = _passwordService.HashPassword("pwd", out salt),
+            PasswordSalt = salt,
+            CreationDate = _dateTimeService.DateTimeOffsetNow,
+            LastSignedIn = _dateTimeService.DateTimeOffsetNow
+        };
+
+        var pac01 = new UserAccount
+        {
+            UserAccountId = Guid.Parse("3eadb218-b95c-48e9-886c-7859de74ba76"),
+            User = new User
+            {
+                Username = "pac",
+                EmailAddress = "pac.first@mail.me",
+                UserRole = UserRole.PlanningAssetCoverage,
+                UserPrivileges = new List<UserPrivilege>()
+                {
+                    UserPrivilege.Viewer,
+                    UserPrivilege.Editor,
+                },
+                UserShift = UserShift.Siang
+            },
+            UserProfile = new UserProfile
+            {
+                FirstName = "Rizky",
+                LastName = "Moneter",
+                DateOfBirth = new DateOnly(year: 1998, month: 11, day: 7)
+            },
+            PasswordHash = _passwordService.HashPassword("pwd", out salt),
+            PasswordSalt = salt,
+            CreationDate = _dateTimeService.DateTimeOffsetNow,
+            LastSignedIn = _dateTimeService.DateTimeOffsetNow
+        };
+
         using var dbContext = _dbContextFactory.CreateDbContext();
 
         dbContext.UserAccounts.Add(administrator01);
+        dbContext.UserAccounts.Add(helpdesk01);
+        dbContext.UserAccounts.Add(pac01);
+
         return await dbContext.SaveChangesAsync();
     }
 }
