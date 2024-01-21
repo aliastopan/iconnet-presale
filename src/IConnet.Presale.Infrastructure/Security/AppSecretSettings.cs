aacfd9edbb23b8eb6da5a4dd19faf8b366beff4f
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-21 20:31:42 +0700

    add jwt options to 'AppSecret' settings

--- src/IConnet.Presale.Infrastructure/Security/AppSecretSettings.cs
+++ src/IConnet.Presale.Infrastructure/Security/AppSecretSettings.cs
@@ -7,6 +7,8 @@ public class AppSecretSettings
         public const string MasterKey = "AppSecrets:MasterKey";
         public const string ConnectionString = "AppSecrets:ConnectionString";
         public const string RedisConnection = "AppSecrets:RedisConnection";
+        public const string JwtIssuer = "AppSecrets:JwtIssuer";
+        public const string JwtAudience = "AppSecrets:JwtAudience";
     }
 
     public const string SectionName = "AppSecrets";
@@ -14,4 +16,9 @@ public class AppSecretSettings
     public string MasterKey { get; set; } = "";
     public string ConnectionString { get; set; } = "";
     public string RedisConnection { get; set; } = "";
+
+    public string JwtIssuer { get; init; } = null!;
+    public string JwtAudience { get; init; } = null!;
+    public TimeSpan JwtLifeTime { get; init; }
+    public TimeSpan JwtRefreshLifeTime { get; init; }
 }
