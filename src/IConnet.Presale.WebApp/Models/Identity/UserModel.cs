773ecd6f8705e62021ade5bb5425608fc4abcac4
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-26 14:28:10 +0700

    refactor: convert property into corresponding enum

--- src/IConnet.Presale.WebApp/Models/Identity/UserModel.cs
+++ src/IConnet.Presale.WebApp/Models/Identity/UserModel.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using IConnet.Presale.Domain.Enums;
 using IConnet.Presale.Infrastructure.Security;
 
 namespace IConnet.Presale.WebApp.Models.Identity;
@@ -8,15 +9,30 @@ public sealed class UserModel
     public UserModel(ClaimsPrincipal principal)
     {
         Username = principal.FindFirst(c => c.Type == JwtClaimTypes.UniqueName)!.Value;
-        Role = principal.FindFirst(c => c.Type == JwtClaimTypes.Role)!.Value;
-        Privileges = principal.FindAll(c => c.Type == JwtClaimTypes.Privileges)
+
+        var employmentStatus = principal.FindFirst(c => c.Type == JwtClaimTypes.EmploymentStatus)!.Value;
+        EmploymentStatus = (EmploymentStatus)Enum.Parse(typeof(EmploymentStatus), employmentStatus);
+
+        var userRole = principal.FindFirst(c => c.Type == JwtClaimTypes.Role)!.Value;
+        Role = (UserRole)Enum.Parse(typeof(UserRole), userRole);
+
+        var privileges = principal.FindAll(c => c.Type == JwtClaimTypes.Privileges)
             .Select(c => c.Value)
             .ToList();
+
+        foreach (var userPrivilege in privileges)
+        {
+            var privilege = (UserPrivilege)Enum.Parse(typeof(UserPrivilege), userPrivilege);
+            Privileges.Add(privilege);
+        }
+
         JobTitle = principal.FindFirst(c => c.Type == JwtClaimTypes.JobTitle)!.Value;
     }
 
     public string Username { get; set; }
-    public string Role { get; set; }
-    public ICollection<string> Privileges { get; set; }
+    public EmploymentStatus EmploymentStatus { get; set; }
+    public UserRole Role { get; set; }
+    public ICollection<UserPrivilege> Privileges { get; set; } = [];
     public string JobTitle { get; set; }
 }
+
