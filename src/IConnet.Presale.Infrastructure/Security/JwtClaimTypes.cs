dabca35799c6fd0e322e2b3b19ae96382081205f
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-26 14:11:09 +0700

    add 'EmploymentStatus' claim

--- src/IConnet.Presale.Infrastructure/Security/JwtClaimTypes.cs
+++ src/IConnet.Presale.Infrastructure/Security/JwtClaimTypes.cs
@@ -17,6 +17,7 @@ public static class JwtClaimTypes
     public const string UniqueName = "unique_name";
     public const string Privileges = "privileges";
     public const string Shift = "shift";
+    public const string EmploymentStatus = "employment_status";
     public const string Role = "role";
     public const string JobTitle = "job_title";
 }
