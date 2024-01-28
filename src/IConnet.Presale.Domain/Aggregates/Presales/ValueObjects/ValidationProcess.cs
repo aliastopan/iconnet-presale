c6e901d7be32ea705489e27528408ea62837b61f
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 02:28:52 +0700

    refactor: 'ValidationProcess' implements 'ValueObject'

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ValidationProcess.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ValidationProcess.cs
@@ -2,7 +2,7 @@
 
 namespace IConnet.Presale.Domain.Aggregates.Presales.ValueObjects;
 
-public class ValidationProcess
+public class ValidationProcess : ValueObject
 {
     public ValidationProcess()
     {
@@ -11,10 +11,31 @@ public class ValidationProcess
         ParameterValidasi = new ValidationParameter();
     }
 
-    public ActionSignature ChatCallMulai { get; set;}
-    public ActionSignature ChatCallRespons { get; set;}
-    public string LinkRecapChatHistory { get; set; }
-    public ValidationParameter ParameterValidasi { get; set; }
-    public ValidationStatus StatusValidasi { get; set; }
-    public string Keterangan { get; set; }
+    public ValidationProcess(ActionSignature chatCallMulai, ActionSignature chatCallRespons, string linkRecapChatHistory,
+        ValidationParameter parameterValidasi, ValidationStatus statusValidasi, string keterangan)
+    {
+        ChatCallMulai = chatCallMulai;
+        ChatCallRespons = chatCallRespons;
+        LinkRecapChatHistory = linkRecapChatHistory;
+        ParameterValidasi = parameterValidasi;
+        StatusValidasi = statusValidasi;
+        Keterangan = keterangan;
+    }
+
+    public ActionSignature ChatCallMulai { get; init;}
+    public ActionSignature ChatCallRespons { get; init;}
+    public string LinkRecapChatHistory { get; init; }
+    public ValidationParameter ParameterValidasi { get; init; }
+    public ValidationStatus StatusValidasi { get; init; }
+    public string Keterangan { get; init; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return ChatCallMulai;
+        yield return ChatCallRespons;
+        yield return LinkRecapChatHistory;
+        yield return ParameterValidasi;
+        yield return StatusValidasi;
+        yield return Keterangan;
+    }
 }
