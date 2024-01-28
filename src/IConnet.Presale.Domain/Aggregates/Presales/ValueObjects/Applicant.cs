e03999573f0a464a8a6175dfbd09a9d93cf249f0
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 02:19:56 +0700

    refactor: 'Applicant' implements 'ValueObject'

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/Applicant.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/Applicant.cs
@@ -3,17 +3,47 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IConnet.Presale.Domain.Aggregates.Presales.ValueObjects;
 
-public class Applicant
+public class Applicant : ValueObject
 {
-    public string NamaLengkap { get; set; }
-    public string IdPln { get; set; }
-    public string Email { get; set; }
-    public string NomorTelepon { get; set; }
-    public string Nik { get; set; }
-    public string Npwp { get; set; }
-    public string Keterangan { get; set; }
-    public string Alamat { get; set; }
+    public Applicant()
+    {
+
+    }
+
+    public Applicant(string namaLengkap, string idPln, string email, string nomorTelepon,
+        string nik, string npwp, string keterangan, string alamat)
+    {
+        NamaLengkap = namaLengkap;
+        IdPln = idPln;
+        Email = email;
+        NomorTelepon = nomorTelepon;
+        Nik = nik;
+        Npwp = npwp;
+        Keterangan = keterangan;
+        Alamat = alamat;
+    }
+
+    public string NamaLengkap { get; init; }
+    public string IdPln { get; init; }
+    public string Email { get; init; }
+    public string NomorTelepon { get; init; }
+    public string Nik { get; init; }
+    public string Npwp { get; init; }
+    public string Keterangan { get; init; }
+    public string Alamat { get; init; }
 
     [NotMapped]
     public string WhatsApp => $"wa.me/{(NomorTelepon.StartsWith('+') ? NomorTelepon[1..] : NomorTelepon)}";
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return NamaLengkap;
+        yield return IdPln;
+        yield return Email;
+        yield return NomorTelepon;
+        yield return Nik;
+        yield return Npwp;
+        yield return Keterangan;
+        yield return Alamat;
+    }
 }
