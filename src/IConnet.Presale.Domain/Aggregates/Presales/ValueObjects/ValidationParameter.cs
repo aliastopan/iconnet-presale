82da411db9bcd40f8304a907ecd946cff89a14b1
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 01:30:58 +0700

    refactor: 'ValidationParameter' implements 'ValueObject'

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ValidationParameter.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ValidationParameter.cs
@@ -2,17 +2,38 @@
 
 namespace IConnet.Presale.Domain.Aggregates.Presales.ValueObjects;
 
-public class ValidationParameter
+public class ValidationParameter : ValueObject
 {
     public ValidationParameter()
     {
         ShareLoc = new Coordinate();
     }
 
-    public ValidationStatus PlnId { get; set; }
-    public ValidationStatus NamaPelanggan { get; set; }
-    public ValidationStatus NomorTelepon { get; set; }
-    public ValidationStatus Email { get; set; }
-    public ValidationStatus AlamatPelanggan { get; set; }
-    public Coordinate ShareLoc { get; set; }
+    public ValidationParameter(ValidationStatus plnId, ValidationStatus namaPelanggan, ValidationStatus nomorTelepon,
+        ValidationStatus email, ValidationStatus alamatPelanggan, Coordinate shareLoc)
+    {
+        PlnId = plnId;
+        NamaPelanggan = namaPelanggan;
+        NomorTelepon = nomorTelepon;
+        Email = email;
+        AlamatPelanggan = alamatPelanggan;
+        ShareLoc = shareLoc;
+    }
+
+    public ValidationStatus PlnId { get; init; }
+    public ValidationStatus NamaPelanggan { get; init; }
+    public ValidationStatus NomorTelepon { get; init; }
+    public ValidationStatus Email { get; init; }
+    public ValidationStatus AlamatPelanggan { get; init; }
+    public Coordinate ShareLoc { get; init; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return PlnId;
+        yield return NamaPelanggan;
+        yield return NomorTelepon;
+        yield return Email;
+        yield return AlamatPelanggan;
+        yield return ShareLoc;
+    }
 }
