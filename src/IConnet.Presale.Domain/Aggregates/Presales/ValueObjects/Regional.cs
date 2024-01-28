b3d974d9d44c215785c245579abab4c6523cb849
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 01:16:47 +0700

    refactor: 'Regional' implements 'ValueObject'

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/Regional.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/Regional.cs
@@ -2,11 +2,23 @@
 
 namespace IConnet.Presale.Domain.Aggregates.Presales.ValueObjects;
 
-public class Regional
+public class Regional : ValueObject
 {
     public Regional()
     {
         Koordinat = new Coordinate();
+    }
+
+    public Regional(string bagian, string kantorPerwakilan, string provinsi,
+        string kabupaten, string kecamatan, string kelurahan, Coordinate koordinat)
+    {
+        Bagian = bagian;
+        KantorPerwakilan = kantorPerwakilan;
+        Provinsi = provinsi;
+        Kabupaten = kabupaten;
+        Kecamatan = kecamatan;
+        Kelurahan = kelurahan;
+        Koordinat = koordinat;
     }
 
     public string Bagian { get; set; }
@@ -16,4 +28,15 @@ public class Regional
     public string Kecamatan { get; set; }
     public string Kelurahan { get; set; }
     public Coordinate Koordinat { get; set; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Bagian;
+        yield return KantorPerwakilan;
+        yield return Provinsi;
+        yield return Kabupaten;
+        yield return Kecamatan;
+        yield return Kelurahan;
+        yield return Koordinat;
+    }
 }
