bef0d79e3c48199fe8cc3a84214ea8bfbb6b4bd5
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 02:34:01 +0700

    refactor: 'ApprovalProcess' implements 'ValueObject'

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ApprovalProcess.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ApprovalProcess.cs
@@ -2,12 +2,38 @@
 
 namespace IConnet.Presale.Domain.Aggregates.Presales.ValueObjects;
 
-public class ApprovalProcess
+public class ApprovalProcess : ValueObject
 {
-    public string StatusApproval { get; set; }
-    public string RootCause { get; set; }
-    public string Keterangan { get; set; }
-    public string JarakShareLoc { get; set; }
-    public string JarakICrmPlus { get; set; }
-    public DateTime VaTerbit { get; set; }
+    public ApprovalProcess()
+    {
+
+    }
+
+    public ApprovalProcess(string statusApproval, string rootCause, string keterangan,
+        string jarakShareLoc, string jarakICrmPlus, DateTime vaTerbit)
+    {
+        StatusApproval = statusApproval;
+        RootCause = rootCause;
+        Keterangan = keterangan;
+        JarakShareLoc = jarakShareLoc;
+        JarakICrmPlus = jarakICrmPlus;
+        VaTerbit = vaTerbit;
+    }
+
+    public string StatusApproval { get; init; }
+    public string RootCause { get; init; }
+    public string Keterangan { get; init; }
+    public string JarakShareLoc { get; init; }
+    public string JarakICrmPlus { get; init; }
+    public DateTime VaTerbit { get; init; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return StatusApproval;
+        yield return RootCause;
+        yield return Keterangan;
+        yield return JarakShareLoc;
+        yield return JarakICrmPlus;
+        yield return VaTerbit;
+    }
 }
