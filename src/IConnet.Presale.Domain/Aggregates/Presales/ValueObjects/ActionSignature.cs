ceca10a3eb80e83977a603c9995e75a32da0c245
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 02:14:59 +0700

    refactor: 'ActionSignature' implements 'ValueObject'

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ActionSignature.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ActionSignature.cs
@@ -2,9 +2,28 @@
 
 namespace IConnet.Presale.Domain.Aggregates.Presales.ValueObjects;
 
-public class ActionSignature
+public class ActionSignature : ValueObject
 {
-    public Guid AccountIdSignature { get; set; }
-    public string Alias { get; set; }
-    public DateTime TglAksi { get; set; }
+    public ActionSignature()
+    {
+
+    }
+
+    public ActionSignature(Guid accountIdSignature, string alias, DateTime tglAksi)
+    {
+        AccountIdSignature = accountIdSignature;
+        Alias = alias;
+        TglAksi = tglAksi;
+    }
+
+    public Guid AccountIdSignature { get; init; }
+    public string Alias { get; init; }
+    public DateTime TglAksi { get; init; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return AccountIdSignature;
+        yield return Alias;
+        yield return TglAksi;
+    }
 }
