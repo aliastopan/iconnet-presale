2855621ca49b2336094ac2046bb83347e74dd930
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 02:08:33 +0700

    refactor: 'PersonInCharge' implements 'ValueObject'

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/PersonInCharge.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/PersonInCharge.cs
@@ -2,8 +2,25 @@
 
 namespace IConnet.Presale.Domain.Aggregates.Presales.ValueObjects;
 
-public class PersonInCharge
+public class PersonInCharge : ValueObject
 {
-    public string Helpdesk { get; set; }
-    public string PlanningAssetCoverage { get; set; }
+    public PersonInCharge()
+    {
+
+    }
+
+    public PersonInCharge(string helpdesk, string planningAssetCoverage)
+    {
+        Helpdesk = helpdesk;
+        PlanningAssetCoverage = planningAssetCoverage;
+    }
+
+    public string Helpdesk { get; init; }
+    public string PlanningAssetCoverage { get; init; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Helpdesk;
+        yield return PlanningAssetCoverage;
+    }
 }
