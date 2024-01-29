97d8c79fddf7fc5ee7267191bdfe95048e1e1ade
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 14:49:45 +0700

    add '-WithSeconds' options

--- src/IConnet.Presale.WebApp/Extensions/TimeSpanExtensions.cs
+++ src/IConnet.Presale.WebApp/Extensions/TimeSpanExtensions.cs
@@ -14,4 +14,22 @@ public static class TimeSpanExtensions
 
         return $"{hours} Jam {minutes} Menit";
     }
+
+    public static string AsReadableDateTimeWithSeconds(this TimeSpan timeSpan)
+    {
+        int seconds = timeSpan.Seconds;
+        int minutes = timeSpan.Minutes;
+        int hours = timeSpan.Hours;
+
+        if (timeSpan.TotalHours < 1)
+        {
+            if (timeSpan.TotalMinutes < 1)
+            {
+                return $"{seconds} Detik";
+            }
+            return $"{minutes} Menit {seconds} Detik";
+        }
+
+        return $"{hours} Jam {minutes} Menit {seconds} Detik";
+    }
 }
