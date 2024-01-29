8e1c88de9a8bd9b75f1f2568651b9c37d73b3fc1
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-29 10:58:53 +0700

    refactor: arrange args order

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/Applicant.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/Applicant.cs
@@ -10,40 +10,40 @@ public class Applicant : ValueObject
 
     }
 
-    public Applicant(string namaLengkap, string idPln, string email, string nomorTelepon,
-        string nik, string npwp, string keterangan, string alamat)
+    public Applicant(string idPln, string namaLengkap, string nomorTelepon, string email,
+        string alamat, string nik, string npwp, string keterangan)
     {
-        NamaLengkap = namaLengkap;
         IdPln = idPln;
-        Email = email;
+        NamaLengkap = namaLengkap;
         NomorTelepon = nomorTelepon;
+        Email = email;
+        Alamat = alamat;
         Nik = nik;
         Npwp = npwp;
         Keterangan = keterangan;
-        Alamat = alamat;
     }
 
-    public string NamaLengkap { get; init; }
     public string IdPln { get; init; }
-    public string Email { get; init; }
+    public string NamaLengkap { get; init; }
     public string NomorTelepon { get; init; }
+    public string Email { get; init; }
+    public string Alamat { get; init; }
     public string Nik { get; init; }
     public string Npwp { get; init; }
     public string Keterangan { get; init; }
-    public string Alamat { get; init; }
 
     [NotMapped]
     public string WhatsApp => $"wa.me/{(NomorTelepon.StartsWith('+') ? NomorTelepon[1..] : NomorTelepon)}";
 
     protected override IEnumerable<object> GetEqualityComponents()
     {
-        yield return NamaLengkap;
         yield return IdPln;
-        yield return Email;
+        yield return NamaLengkap;
         yield return NomorTelepon;
+        yield return Email;
+        yield return Alamat;
         yield return Nik;
         yield return Npwp;
         yield return Keterangan;
-        yield return Alamat;
     }
 }
