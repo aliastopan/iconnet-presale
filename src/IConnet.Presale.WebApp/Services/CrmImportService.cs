228ac0a11705a779ec967a5c1a768b9bd96ffa88
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-30 03:01:22 +0700

    add async overload

--- src/IConnet.Presale.WebApp/Services/CrmImportService.cs
+++ src/IConnet.Presale.WebApp/Services/CrmImportService.cs
@@ -96,6 +96,79 @@ public sealed class CrmImportService
         };
     }
 
+    public async Task<(List<IApprovalOpportunityModel> crmModels, CrmImportMetadata crmMetadata)> ImportAsync(string input)
+    {
+        string[] contents = SplitBySpecialCharacters(input);
+        var importMetadata = GetImportMetadata(input, contents);
+
+        bool isValid = contents.Length % NumberOfColumn == 0;
+        if (!isValid)
+        {
+            Log.Warning("Invalid row count of {0}", contents.Length);
+            return (new List<IApprovalOpportunityModel>(), new CrmImportMetadata());
+        }
+
+        int numberOfRows = contents.Length / NumberOfColumn;
+        var importModels = new List<ImportModel>();
+
+        for (int i = 0; i < numberOfRows; i++)
+        {
+            string[] rowData = contents.Skip(i * NumberOfColumn).Take(NumberOfColumn).ToArray();
+            var importModel = await CreateImportModelAsync(rowData);
+
+            var hasDuplicate = _importModels.Any(crm => crm.IdPermohonan == importModel.IdPermohonan);
+            if (hasDuplicate)
+            {
+                importMetadata.NumberOfDuplicates++;
+                continue;
+            }
+
+            importModels.Add(importModel);
+        }
+
+        _importModels.AddRange(importModels);
+
+        return (_importModels, importMetadata);
+    }
+
+    private async Task<ImportModel> CreateImportModelAsync(string[] column)
+    {
+        return new ImportModel
+        {
+            IdPermohonan = column[0],
+            TglPermohonan = column[1],
+            DurasiTidakLanjut = column[2],
+            NamaPemohon = column[3],
+            IdPln = column[4],
+            Layanan = column[5],
+            SumberPermohonan = column[6],
+            StatusPermohonan = column[7],
+            NamaAgen = column[8],
+            EmailAgen = column[9],
+            TeleponAgen = column[10],
+            MitraAgen = column[11],
+            Splitter = column[12],
+            JenisPermohonan = column[13],
+            TeleponPemohon = column[14],
+            EmailPemohon = column[15],
+            NikPemohon = column[16],
+            NpwpPemohon = column[17],
+            Keterangan = column[18],
+            AlamatPemohon = column[19],
+            Regional = column[20],
+            KantorPerwakilan = column[21],
+            Provinsi = column[22],
+            Kabupaten = column[23],
+            Kecamatan = column[24],
+            Kelurahan = column[25],
+            Latitude = column[26],
+            Longitude = column[27],
+            TglImport = _dateTimeService.DateTimeOffsetNow.DateTime,
+            ImportAccountIdSignature = await _sessionService.GetUserAccountIdAsync(),
+            ImportAliasSignature = _sessionService.GetSessionAlias()
+        };
+    }
+
     private static CrmImportMetadata GetImportMetadata(string input, string[] contents)
     {
         return new CrmImportMetadata
