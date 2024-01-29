94eaf965f8b42ed3be9ac0427dc7c3721d65c179
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-30 02:16:48 +0700

    extend 'Session' service functionality

--- src/IConnet.Presale.WebApp/Services/SessionService.cs
+++ src/IConnet.Presale.WebApp/Services/SessionService.cs
@@ -1,4 +1,6 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using IConnet.Presale.WebApp.Models.Identity;
 using IConnet.Presale.Domain.Enums;
 
@@ -6,12 +8,30 @@ namespace IConnet.Presale.WebApp.Services;
 
 public sealed class SessionService
 {
+    private readonly ProtectedLocalStorage _localStorage;
+    private readonly NavigationManager _navigationManager;
+
+    public SessionService(ProtectedLocalStorage localStorage,
+        NavigationManager navigationManager)
+    {
+        _localStorage = localStorage;
+        _navigationManager = navigationManager;
+    }
+
     public UserModel? UserModel { get; private set; }
     public bool HasUser => UserModel is not null;
 
     public void SetSession(ClaimsPrincipal principal)
     {
         UserModel = new UserModel(principal);
+    }
+
+    public async Task SignOutAsync()
+    {
+        await _localStorage.DeleteAsync("access-token");
+        await _localStorage.DeleteAsync("refresh-token");
+
+        _navigationManager.NavigateTo("/", forceLoad: true);
     }
 
     public string GetSessionAlias()
@@ -25,6 +45,32 @@ public sealed class SessionService
         };
     }
 
+    public async Task<string> GetSessionAliasAsync()
+    {
+        if (UserModel is null)
+        {
+            await SignOutAsync();
+        }
+
+        return UserModel switch
+        {
+            { Role: UserRole.Helpdesk } => $"{UserModel!.Username} {GetRoleString("PH")}",
+            { Role: UserRole.PlanningAssetCoverage } => $"{UserModel!.Username} {GetRoleString("PAC")}",
+            { Role: UserRole.Administrator } => $"{UserModel!.Username} {GetRoleString("ADMIN")}",
+            _ => $"{UserModel!.Username} (GUEST)"
+        };
+    }
+
+    public async Task<string> GetJobShiftAsync()
+    {
+        if (UserModel is null)
+        {
+            await SignOutAsync();
+        }
+
+        return UserModel!.JobShift.ToString();
+    }
+
     private string GetRoleString(string role)
     {
         if (UserModel!.EmploymentStatus == EmploymentStatus.Intern)
@@ -34,5 +80,4 @@ public sealed class SessionService
 
         return $"({role})";
     }
-
 }
