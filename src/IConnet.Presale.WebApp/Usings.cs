599378ecbd6e6f9d7285e2d29f4fe3b59bdf3422
Taufan Augusta <taufan.augusta@protonmail.com>
2024-01-30 18:13:10 +0700

    add global usings

--- src/IConnet.Presale.WebApp/Usings.cs
+++ src/IConnet.Presale.WebApp/Usings.cs
@@ -1,5 +1,13 @@
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.FluentUI.AspNetCore.Components;
+global using Microsoft.JSInterop;
+
 global using IConnet.Presale.Application.Common.Interfaces.Managers;
 global using IConnet.Presale.Application.Common.Interfaces.Services;
+global using IConnet.Presale.Shared.Interfaces.Models.Presales;
+global using IConnet.Presale.WebApp.Helpers;
+global using IConnet.Presale.WebApp.Services;
+
 
 global using AnnotatedResult;
 global using Serilog;
