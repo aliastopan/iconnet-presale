9c3812baaa11938e2d8c548b3e9e53ef53fbf45d
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-01 17:31:46 +0700

    add `Helpdesk` page

---
+++ src/IConnet.Presale.WebApp/Components/Pages/HelpdeskPageBase.cs
@@ -0,0 +1,16 @@
+namespace IConnet.Presale.WebApp.Components.Pages;
+
+public class HelpdeskPageBase : WorkloadPageBase
+{
+    [Inject] public IDateTimeService DateTimeService { get; set; } = default!;
+
+    private readonly string _pageName = "Helpdesk page";
+
+    protected override async Task OnInitializedAsync()
+    {
+        PageName = _pageName;
+        CacheFetchMode = CacheFetchMode.OnlyImportVerified; // needs only claimed fetch mode
+
+        await base.OnInitializedAsync();
+    }
+}
