8c5776aa494d681e902716acb3a4fc60e53958d5
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-02 17:41:31 +0700

    refactor: break `CrmVerificationDialog` into partial class

---
+++ src/IConnet.Presale.WebApp/Components/Dialogs/CrmVerificationDialog.razor.cs
@@ -0,0 +1,54 @@
+namespace IConnet.Presale.WebApp.Components.Dialogs;
+
+public partial class CrmVerificationDialog
+{
+    [Inject] public IDateTimeService DateTimeService { get; set; } = default!;
+    [Inject] public SessionService SessionService { get; set; } = default!;
+
+    private bool _isCrmValid = false;
+
+    [Parameter]
+    public WorkPaper Content { get; set; } = default!;
+
+    [CascadingParameter]
+    public FluentDialog Dialog { get; set; } = default!;
+
+    private async Task SaveAsync()
+    {
+        await VerifyCrmAsync();
+        await Dialog.CloseAsync(Content);
+    }
+
+    private async Task DeleteAsync()
+    {
+        await DeleteCrmAsync();
+        await Dialog.CloseAsync(Content);
+    }
+
+    private async Task CancelAsync()
+    {
+        await Dialog.CancelAsync();
+    }
+
+    private async Task VerifyCrmAsync()
+    {
+        Content.ApprovalOpportunity.StatusImport = ImportStatus.Verified;
+        Content.ApprovalOpportunity.ImportVerifikasiSignature = new ActionSignature
+        {
+            AccountIdSignature = SessionService.UserModel!.UserAccountId,
+            Alias = await SessionService.GetSessionAliasAsync(),
+            TglAksi = DateTimeService.DateTimeOffsetNow.DateTime
+        };
+    }
+
+    private async Task DeleteCrmAsync()
+    {
+        Content.ApprovalOpportunity.StatusImport = ImportStatus.Invalid;
+        Content.ApprovalOpportunity.ImportVerifikasiSignature = new ActionSignature
+        {
+            AccountIdSignature = SessionService.UserModel!.UserAccountId,
+            Alias = await SessionService.GetSessionAliasAsync(),
+            TglAksi = DateTimeService.DateTimeOffsetNow.DateTime
+        };
+    }
+}
