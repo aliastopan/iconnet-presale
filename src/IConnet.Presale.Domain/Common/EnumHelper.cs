8d7da1ceca1953d2b6140c4d45ecc610b62e26e5
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-02 20:05:57 +0700

    refactor: set enum names as display text

--- src/IConnet.Presale.Domain/Common/EnumHelper.cs
+++ src/IConnet.Presale.Domain/Common/EnumHelper.cs
@@ -1,14 +1,23 @@
+using System.Text.RegularExpressions;
+
 namespace IConnet.Presale.Domain.Common;
 
 public class EnumHelper
 {
     public static IEnumerable<string> GetIEnumerable<T>(bool skipFirst = false) where T : Enum
     {
+        var enumNames = Enum.GetNames(typeof(T));
+
         if (!skipFirst)
         {
-            return Enum.GetNames(typeof(T));
+            return enumNames.Select(EnumToDisplayString);
         }
 
-        return Enum.GetNames(typeof(T)).Skip(1);
+        return enumNames.Select(EnumToDisplayString).Skip(1);
+    }
+
+    private static string EnumToDisplayString(string enumValue)
+    {
+        return Regex.Replace(enumValue, "([a-z])([A-Z])", "$1 $2");
     }
 }
