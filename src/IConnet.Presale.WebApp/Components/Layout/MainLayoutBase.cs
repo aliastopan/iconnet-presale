983a283d79912fbd411ee84c21ac1bbf62f7502b
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-04 16:27:47 +0700

    refactor: introduce `MainLayoutBase`

---
+++ src/IConnet.Presale.WebApp/Components/Layout/MainLayoutBase.cs
@@ -0,0 +1,30 @@
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using IConnet.Presale.Shared.Validations;
+
+namespace IConnet.Presale.WebApp.Components.Layout;
+
+public class MainLayoutBase : LayoutComponentBase
+{
+    [Inject] public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+    [Inject] public NavigationManager NavigationManager { get; set; } = default!;
+    [Inject] public ProtectedLocalStorage LocalStorage { get; set; } = default!;
+    [Inject] public SessionService SessionService { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await AuthenticationStateProvider.GetAuthenticationStateAsync();
+    }
+
+    protected void Redirect()
+    {
+        var homePage = new Regex(RegexPattern.HomePageUrl);
+        var currentUrl = NavigationManager.Uri;
+
+        if (!homePage.IsMatch(currentUrl))
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
