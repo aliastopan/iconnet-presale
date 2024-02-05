51ddacc123b611398ab4dd3184efd4272f9e4667
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-05 11:23:15 +0700

    add `OnClick`

--- src/IConnet.Presale.WebApp/Components/Layout/NavMenuBase.cs
+++ src/IConnet.Presale.WebApp/Components/Layout/NavMenuBase.cs
@@ -3,4 +3,9 @@ namespace IConnet.Presale.WebApp.Components.Layout;
 public class NavMenuBase : LayoutComponentBase
 {
     public bool IsExpanded { get; set; } = true;
+
+    public void OnClick()
+    {
+        Log.Warning("OnClick");
+    }
 }
