32893094862015d1a88a985edd7945e86bd2d5fe
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-05 10:49:06 +0700

    add static `PageRoute`

---
+++ src/IConnet.Presale.WebApp/Components/Pages/PageRoute.cs
@@ -0,0 +1,12 @@
+namespace IConnet.Presale.WebApp.Components.Pages;
+
+public static class PageRoute
+{
+    public const string Home = "/";
+    public const string CrmImport = "/crm-import";
+    public const string CrmImportVerification = "/crm-verification";
+    public const string WorkloadShared = "/workload-shared";
+    public const string HelpdeskStaging = "/helpdesk-staging";
+    public const string Helpdesk = "/helpdesk";
+    public const string Settings = "/settings";
+}
