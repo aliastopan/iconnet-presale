bce7988f88c5e53a77be200c3e081e0e9e834263
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-05 22:36:07 +0700

    refactor: tab content as button

--- src/IConnet.Presale.WebApp/Components/Custom/TabNavBase.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/TabNavBase.cs
@@ -4,4 +4,9 @@ public class TabNavBase : ComponentBase
 {
     [Parameter]
     public TabNavigation Tab { get; set; }
+
+    protected void CloseTab()
+    {
+        Log.Warning("Closing {0}", Tab.Id);
+    }
 }
