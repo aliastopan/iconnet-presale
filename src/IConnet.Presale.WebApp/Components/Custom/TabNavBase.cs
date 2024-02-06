e1b4ccda4e03e67fe8256899c38a6685bac0a486
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-06 17:10:14 +0700

    refactor: changing `TabNav`

--- src/IConnet.Presale.WebApp/Components/Custom/TabNavBase.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/TabNavBase.cs
@@ -2,11 +2,14 @@ namespace IConnet.Presale.WebApp.Components.Custom;
 
 public class TabNavBase : ComponentBase
 {
-    [Parameter]
-    public TabNavigation Tab { get; set; }
+    [Parameter] public TabNavigation Tab { get; set; }
+    [Parameter] public EventCallback<TabNavigation> OnTabChange { get; set; }
+    [Parameter] public EventCallback<TabNavigation> OnTabClick { get; set; }
 
-    [Parameter]
-    public EventCallback<TabNavigation> OnTabClick { get; set; }
+    protected async Task ChangeTabAsync()
+    {
+        await OnTabChange.InvokeAsync(Tab);
+    }
 
     protected async Task ClickTabAsync()
     {
--- src/IConnet.Presale.WebApp/Components/Layout/TabControl.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Layout/TabControl.razor.cs
@@ -5,6 +5,11 @@ public partial class TabControl
     [Parameter]
     public TabNavigationManager TabNavigationManager { get; set; } = default!;
 
+    public void ChangeTab(TabNavigation tab)
+    {
+        TabNavigationManager.ChangeTab(tab);
+    }
+
     public void SelectTab(TabNavigation tab)
     {
         Log.Warning("Tab selected: {0}", tab.Label);
