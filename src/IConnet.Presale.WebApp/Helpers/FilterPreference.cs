3f2a345cd30c14966122d9fbd44dd67bdf3f211d
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-12 17:00:02 +0700

    add boolean `ShowFilters`

--- src/IConnet.Presale.WebApp/Helpers/FilterPreference.cs
+++ src/IConnet.Presale.WebApp/Helpers/FilterPreference.cs
@@ -3,4 +3,10 @@ namespace IConnet.Presale.WebApp.Helpers;
 public class FilterPreference
 {
     public string KantorPerwakilan { get; set; } = EnumerableOptions.KantorPerwakilan.First();
+    public bool ShowFilters { get; set; } = true;
+
+    public void ToggleFilters()
+    {
+        ShowFilters = !ShowFilters;
+    }
 }
