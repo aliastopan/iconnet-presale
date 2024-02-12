b78e066220366790367f87f56e39566a3c7a6935
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-12 16:01:50 +0700

    feat: session based `FilterPreference`

--- src/IConnet.Presale.WebApp/Components/Forms/FilterForm.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Forms/FilterForm.razor.cs
@@ -2,15 +2,24 @@ namespace IConnet.Presale.WebApp.Components.Forms;
 
 public partial class FilterForm
 {
+    [Inject] public SessionService SessionService { get; set; } = default!;
+
     [Parameter] public EventCallback OnFilter { get; set; }
 
     private readonly WorkloadFilter _filter = new WorkloadFilter();
     public WorkloadFilter Filter => _filter;
 
+    protected override async Task OnInitializedAsync()
+    {
+        _filter.FilterOffice = SessionService.FilterPreference.KantorPerwakilan;
+        await OnFilter.InvokeAsync();
+    }
+
     public async Task FilterByOfficeAsync(string filterOffice)
     {
         Filter.FilterSearch = string.Empty;
         Filter.FilterOffice = filterOffice;
+        SessionService.FilterPreference.KantorPerwakilan = filterOffice;
 
         await OnFilter.InvokeAsync();
     }
@@ -19,6 +28,7 @@ public partial class FilterForm
     {
         Filter.ResetColumnFilters();
         Filter.FilterOffice = Filter.FilterOfficeDefault;
+        SessionService.FilterPreference.KantorPerwakilan = Filter.FilterOffice;
         Filter.FilterSearch = filterSearch;
 
         await OnFilter.InvokeAsync();
