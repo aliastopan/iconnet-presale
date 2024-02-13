b3635b6b928f9d93e066ca1d92a953e97f66e71d
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-13 23:04:09 +0700

    add `ErrorBoundary` recovery

--- src/IConnet.Presale.WebApp/Components/Layout/MainLayoutBase.cs
+++ src/IConnet.Presale.WebApp/Components/Layout/MainLayoutBase.cs
@@ -1,6 +1,7 @@
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components.Web;
 using IConnet.Presale.Shared.Validations;
 
 namespace IConnet.Presale.WebApp.Components.Layout;
@@ -12,6 +13,13 @@ public class MainLayoutBase : LayoutComponentBase
     [Inject] public TabNavigationManager TabNavigationManager { get; set; } = default!;
     [Inject] public ProtectedLocalStorage LocalStorage { get; set; } = default!;
     [Inject] public SessionService SessionService { get; set; } = default!;
+
+    public ErrorBoundary? ErrorBoundary { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        ErrorBoundary?.Recover();
+    }
 
     protected override async Task OnInitializedAsync()
     {
