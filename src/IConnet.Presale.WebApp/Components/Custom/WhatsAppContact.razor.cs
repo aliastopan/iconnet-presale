40e2fe4b39ee301b59a7e8ecaeba178c8fc29de3
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-18 15:27:05 +0700

    feat: WhatsApp contact navigation

--- src/IConnet.Presale.WebApp/Components/Custom/WhatsAppContact.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/WhatsAppContact.razor.cs
@@ -2,10 +2,15 @@ namespace IConnet.Presale.WebApp.Components.Custom;
 
 public partial class WhatsAppContact : ComponentBase
 {
+    [Inject] public IJSRuntime JsRuntime { get; set; } = default!;
+
     [Parameter] public string WhatsApp { get; set; } = string.Empty;
 
-    protected void OnContact()
+    protected async Task OnContactAsync()
     {
         LogSwitch.Debug("WhatsApp: {contact}", WhatsApp);
+        var url = $"http://{WhatsApp}";
+
+        await JsRuntime.InvokeVoidAsync("open", url, "_blank");
     }
 }
