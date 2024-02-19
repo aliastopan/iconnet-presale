da576dd2df223d75fbeb4a082cac13a96c96ec38
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-19 12:49:02 +0700

    add ctor `ChatTemplate`

--- src/IConnet.Presale.Domain/Entities/ChatTemplate.cs
+++ src/IConnet.Presale.Domain/Entities/ChatTemplate.cs
@@ -4,6 +4,19 @@ namespace IConnet.Presale.Domain.Entities;
 
 public class ChatTemplate
 {
+    public ChatTemplate()
+    {
+
+    }
+
+    public ChatTemplate(string templateName, int sequence, string content)
+    {
+        ChatTemplateId = Guid.NewGuid();
+        TemplateName = templateName;
+        Sequence = sequence;
+        Content = content;
+    }
+
     public Guid ChatTemplateId { get; set; }
     public string TemplateName { get; set; }
     public int Sequence { get; set; }
