7df7685f8d168e800f67aa16da43ab9de8e58b47
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-19 19:18:30 +0700

    feat: `ChatTemplate`

--- src/IConnet.Presale.WebApp/Components/Dialogs/ChatTemplateDialog.cs
+++ src/IConnet.Presale.WebApp/Components/Dialogs/ChatTemplateDialog.cs
@@ -2,6 +2,8 @@ namespace IConnet.Presale.WebApp.Components.Dialogs;
 
 public partial class ChatTemplateDialog : IDialogContentComponent<WorkPaper>
 {
+    [Inject] public ChatTemplateService ChatTemplateService { get; set; } = default!;
+
     [Parameter]
     public WorkPaper Content { get; set; } = default!;
 
