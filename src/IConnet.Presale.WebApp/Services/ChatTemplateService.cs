27351decf4e510b5bae1b73d8701e825a13169a7
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-19 23:43:24 +0700

    feat: Html format `ChatTemplate`

--- src/IConnet.Presale.WebApp/Services/ChatTemplateService.cs
+++ src/IConnet.Presale.WebApp/Services/ChatTemplateService.cs
@@ -5,14 +5,19 @@ namespace IConnet.Presale.WebApp.Services;
 public class ChatTemplateService
 {
     private readonly List<string> _chatTemplates = new List<string>();
+    private readonly List<MarkupString> _htmlChatTemplates = new List<MarkupString>();
 
-    public IReadOnlyCollection<string> ChatTemplates => _chatTemplates.AsReadOnly();
+    public IReadOnlyCollection<MarkupString> HtmlChatTemplates => _htmlChatTemplates.AsReadOnly();
 
     public void InitializeChatTemplate(ICollection<ChatTemplateDto> chatTemplateDtos)
     {
         foreach (var chatTemplate in chatTemplateDtos)
         {
             _chatTemplates.Add(chatTemplate.Content);
+            _htmlChatTemplates.Add((MarkupString)chatTemplate.Content
+                .FormatHtmlBreak()
+                .FormatHtmlBold()
+                .FormatHtmlItalic());
         }
 
         LogSwitch.Debug("Chat template has been initialized ({length} sequences)", _chatTemplates.Count);
