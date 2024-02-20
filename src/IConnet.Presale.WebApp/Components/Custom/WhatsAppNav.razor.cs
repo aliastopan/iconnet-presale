fdb73492fb76bc4f4316aed3a99be4849b7094b9
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-20 21:00:28 +0700

    refactor: rename `WhatsAppNav`

--- src/IConnet.Presale.WebApp/Components/Custom/WhatsAppNav.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/WhatsAppNav.razor.cs
@@ -1,15 +1,15 @@
 namespace IConnet.Presale.WebApp.Components.Custom;
 
-public partial class WhatsAppContact : ComponentBase
+public partial class WhatsAppNav : ComponentBase
 {
     [Inject] public IJSRuntime JsRuntime { get; set; } = default!;
 
-    [Parameter] public string WhatsApp { get; set; } = string.Empty;
+    [Parameter] public string Contact { get; set; } = string.Empty;
 
     protected async Task OnContactAsync()
     {
-        LogSwitch.Debug("WhatsApp: {contact}", WhatsApp);
-        var url = $"http://{WhatsApp}";
+        LogSwitch.Debug("WhatsApp: {contact}", Contact);
+        var url = $"http://{Contact}";
 
         await JsRuntime.InvokeVoidAsync("open", url, "_blank");
     }
