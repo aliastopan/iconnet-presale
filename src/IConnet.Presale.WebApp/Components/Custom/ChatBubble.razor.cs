77e3e82f37f4efa2c7f8de98bcf6426e43e83c81
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-20 23:22:46 +0700

    feat: copy (clipboard) `ChatTemplate`

--- src/IConnet.Presale.WebApp/Components/Custom/ChatBubble.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/ChatBubble.razor.cs
@@ -4,6 +4,8 @@ namespace IConnet.Presale.WebApp.Components.Custom;
 
 public partial class ChatBubble : ComponentBase
 {
+    [Inject] public IJSRuntime JsRuntime { get; set; } = default!;
+
     [Parameter] public WorkPaper? ActiveWorkPaper { get; set; }
     [Parameter] public ChatTemplateModel ChatTemplate { get; set; } = default!;
     [Parameter] public string Sender { get; set; } = string.Empty;
@@ -26,8 +28,22 @@ public partial class ChatBubble : ComponentBase
             .ReplacePlaceholder(PlaceholderText.Npwp, ActiveWorkPaper.ApprovalOpportunity.Pemohon.Npwp);
     }
 
-    public void CopyChatTemplate()
+    public async Task CopyChatTemplateAsync()
     {
-        LogSwitch.Debug("Copying Chat Template with sequence {index}", ChatTemplate.Sequence);
+        if (ActiveWorkPaper is null)
+        {
+            return;
+        }
+
+        LogSwitch.Debug("Copying clipboard chat {sequence}", ChatTemplate.Sequence);
+
+        await JsRuntime.InvokeVoidAsync("navigator.clipboard.writeText", ChatTemplate.Content
+            .ReplacePlaceholder(PlaceholderText.IdPln, ActiveWorkPaper.ApprovalOpportunity.Pemohon.IdPln)
+            .ReplacePlaceholder(PlaceholderText.NamaPelanggan, ActiveWorkPaper.ApprovalOpportunity.Pemohon.NamaLengkap)
+            .ReplacePlaceholder(PlaceholderText.NomorTelepon, ActiveWorkPaper.ApprovalOpportunity.Pemohon.NomorTelepon)
+            .ReplacePlaceholder(PlaceholderText.AlamatEmail, ActiveWorkPaper.ApprovalOpportunity.Pemohon.Email)
+            .ReplacePlaceholder(PlaceholderText.AlamatPemasangan, ActiveWorkPaper.ApprovalOpportunity.Pemohon.Alamat)
+            .ReplacePlaceholder(PlaceholderText.Nik, ActiveWorkPaper.ApprovalOpportunity.Pemohon.Nik)
+            .ReplacePlaceholder(PlaceholderText.Npwp, ActiveWorkPaper.ApprovalOpportunity.Pemohon.Npwp));
     }
 }
