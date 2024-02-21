ec30f8301750f95f1d3a62ed6f60dbc4230a1a82
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-21 14:42:06 +0700

    add copy-and-update pattern `ValidationParameter`

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ValidationParameter.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ValidationParameter.cs
@@ -27,6 +27,87 @@ public class ValidationParameter : ValueObject
     public ValidationStatus ValidasiAlamat { get; init; } = default;
     public Coordinate ShareLoc { get; init; } = new();
 
+    public ValidationParameter WithValidasiIdPln(ValidationStatus validasiIdPln)
+    {
+        return new ValidationParameter
+        {
+            ValidasiIdPln = validasiIdPln,
+            ValidasiNama = this.ValidasiNama,
+            ValidasiNomorTelepon = this.ValidasiNomorTelepon,
+            ValidasiEmail = this.ValidasiEmail,
+            ValidasiAlamat = this.ValidasiAlamat,
+            ShareLoc = this.ShareLoc
+        };
+    }
+
+    public ValidationParameter WithValidasiNama(ValidationStatus validasiNama)
+    {
+        return new ValidationParameter
+        {
+            ValidasiIdPln = this.ValidasiIdPln,
+            ValidasiNama = validasiNama,
+            ValidasiNomorTelepon = this.ValidasiNomorTelepon,
+            ValidasiEmail = this.ValidasiEmail,
+            ValidasiAlamat = this.ValidasiAlamat,
+            ShareLoc = this.ShareLoc
+        };
+    }
+
+    public ValidationParameter WithValidasiNomorTelepon(ValidationStatus validasiNomorTelepon)
+    {
+        return new ValidationParameter
+        {
+            ValidasiIdPln = this.ValidasiIdPln,
+            ValidasiNama = this.ValidasiNama,
+            ValidasiNomorTelepon = validasiNomorTelepon,
+            ValidasiEmail = this.ValidasiEmail,
+            ValidasiAlamat = this.ValidasiAlamat,
+            ShareLoc = this.ShareLoc
+        };
+    }
+
+
+    public ValidationParameter WithValidasiEmail(ValidationStatus validasiEmail)
+    {
+        return new ValidationParameter
+        {
+            ValidasiIdPln = this.ValidasiIdPln,
+            ValidasiNama = this.ValidasiNama,
+            ValidasiNomorTelepon = this.ValidasiNomorTelepon,
+            ValidasiEmail = validasiEmail,
+            ValidasiAlamat = this.ValidasiAlamat,
+            ShareLoc = this.ShareLoc
+        };
+    }
+
+
+    public ValidationParameter WithValidasiAlamat(ValidationStatus validasiAlamat)
+    {
+        return new ValidationParameter
+        {
+            ValidasiIdPln = this.ValidasiIdPln,
+            ValidasiNama = this.ValidasiNama,
+            ValidasiNomorTelepon = this.ValidasiNomorTelepon,
+            ValidasiEmail = this.ValidasiEmail,
+            ValidasiAlamat = validasiAlamat,
+            ShareLoc = this.ShareLoc
+        };
+    }
+
+
+    public ValidationParameter WithShareLoc(Coordinate shareLoc)
+    {
+        return new ValidationParameter
+        {
+            ValidasiIdPln = this.ValidasiIdPln,
+            ValidasiNama = this.ValidasiNama,
+            ValidasiNomorTelepon = this.ValidasiNomorTelepon,
+            ValidasiEmail = this.ValidasiEmail,
+            ValidasiAlamat = this.ValidasiAlamat,
+            ShareLoc = shareLoc
+        };
+    }
+
     protected override IEnumerable<object> GetEqualityComponents()
     {
         yield return ValidasiIdPln;
