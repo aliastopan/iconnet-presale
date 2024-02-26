5603a8bd61af8dc1fd9601a7584b53fb071ada51
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-26 23:17:30 +0700

    refactor: using `OptionService`

--- src/IConnet.Presale.WebApp/Services/OptionService.cs
+++ src/IConnet.Presale.WebApp/Services/OptionService.cs
@@ -4,7 +4,16 @@ namespace IConnet.Presale.WebApp.Services;
 
 public class OptionService
 {
+    public OptionService()
+    {
+        SearchFilterOptions =
+        [
+            "ID PERMOHONAN"
+        ];
+    }
+
     public ICollection<string> KantorPerwakilanOptions { get; init; } = [];
+    public ICollection<string> SearchFilterOptions { get ; init; } = [];
 
     public void PopulateKantorPerwakilan(ICollection<RepresentativeOfficeDto> representativeOfficeDtos)
     {
