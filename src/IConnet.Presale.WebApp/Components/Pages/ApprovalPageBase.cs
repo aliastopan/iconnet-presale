b55a771ab215e47e7c699912e24c21b9cc4e597f
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-27 15:21:16 +0700

    add `Approval` page

---
+++ src/IConnet.Presale.WebApp/Components/Pages/ApprovalPageBase.cs
@@ -0,0 +1,9 @@
+using IConnet.Presale.WebApp.Components.Dialogs;
+using IConnet.Presale.WebApp.Components.Forms;
+
+namespace IConnet.Presale.WebApp.Components.Pages;
+
+public class ApprovalPageBase : WorkloadPageBase
+{
+
+}
