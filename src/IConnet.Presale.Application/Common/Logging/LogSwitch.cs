e106472cd75cb1df2e6e06710f5a019a5169dd55
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-27 15:32:25 +0700

    add environment guard

--- src/IConnet.Presale.Application/Common/Logging/LogSwitch.cs
+++ src/IConnet.Presale.Application/Common/Logging/LogSwitch.cs
@@ -3,13 +3,20 @@ using Serilog.Core;
 using Serilog.Events;
 
 namespace IConnet.Presale.Application.Common.Logging;
+#pragma warning disable CS0162 // Unreachable code detected
 
 public static class LogSwitch
 {
+    private const bool IsProduction = false;
     public static LoggingLevelSwitch LevelSwitch = new LoggingLevelSwitch();
 
     public static void Debug(string messageTemplate, params object[] propertyValues)
     {
+        if (IsProduction)
+        {
+            return;
+        }
+
         var revertLevel = LevelSwitch.MinimumLevel;
 
         LevelSwitch.MinimumLevel = LogEventLevel.Debug;
@@ -17,3 +24,5 @@ public static class LogSwitch
         LevelSwitch.MinimumLevel = revertLevel;
     }
 }
+
+#pragma warning restore CS0162 // Unreachable code detected
