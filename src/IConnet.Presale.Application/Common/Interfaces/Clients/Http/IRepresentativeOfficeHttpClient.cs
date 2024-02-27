728968f53abfbef0a9e50f385cf183daa872b53f
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-27 10:39:31 +0700

    fix: implement `IHttpClientBase`

--- src/IConnet.Presale.Application/Common/Interfaces/Clients/Http/IRepresentativeOfficeHttpClient.cs
+++ src/IConnet.Presale.Application/Common/Interfaces/Clients/Http/IRepresentativeOfficeHttpClient.cs
@@ -1,6 +1,6 @@
 namespace IConnet.Presale.Application.Common.Interfaces.Clients.Http;
 
-public interface IRepresentativeOfficeHttpClient
+public interface IRepresentativeOfficeHttpClient : IHttpClientBase
 {
     Task<HttpResult> GetRepresentativeOfficesAsync();
 }
