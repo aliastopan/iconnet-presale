4c7b64cc5fb3ac31f1e8b6ab10341503de5453d2
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-28 13:17:03 +0700

    refactor: bit value `WorkPaperLevel`

--- src/IConnet.Presale.Domain/Enums/WorkPaperLevel.cs
+++ src/IConnet.Presale.Domain/Enums/WorkPaperLevel.cs
@@ -3,11 +3,11 @@ namespace IConnet.Presale.Domain.Enums;
 [Flags]
 public enum WorkPaperLevel
 {
-    ImportUnverified,
-    ImportVerified,
-    ImportInvalid,
-    ImportArchived,
-    Validating,
-    WaitingApproval,
-    DoneProcessing
+    ImportUnverified = 1,
+    ImportInvalid = 2,
+    ImportArchived = 4,
+    ImportVerified = 8,
+    Validating = 16,
+    WaitingApproval = 32,
+    DoneProcessing = 64
 }
