86268528e546766a522e9e8298333a3901519fd3
Taufan Augusta <taufan.augusta@protonmail.com>
2024-02-29 19:31:02 +0700

    feat: `CreateUserForm`

--- src/IConnet.Presale.WebApp/Components/Forms/CreateUserForm.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Forms/CreateUserForm.razor.cs
@@ -6,6 +6,8 @@ public partial class CreateUserForm : ComponentBase
 {
     public CreateUserModel CreateUserModel { get; set; } = default!;
 
+    protected Func<string, bool> OptionDisableSuperUser => option => option == OptionSelect.Role.SuperUser;
+
     protected override void OnInitialized()
     {
         CreateUserModel = new CreateUserModel();
