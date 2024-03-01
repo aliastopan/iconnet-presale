159afc4a8bd3a2a10e65c00df3b07774e136130f
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-01 14:12:08 +0700

    introduce `InChargeDuration`

---
+++ src/IConnet.Presale.WebApp/Helpers/InChargeDuration.cs
@@ -0,0 +1,7 @@
+namespace IConnet.Presale.WebApp.Helpers;
+
+public static class InChargeDuration
+{
+    public static readonly TimeSpan ValidationDuration = new TimeSpan(0, 5, 0);
+    public static readonly TimeSpan ApprovalDuration = new TimeSpan(0, 10, 0);
+}
