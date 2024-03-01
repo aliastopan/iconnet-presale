5813b6669ca33c1afaa194e4ca8a4170c1971982
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-01 16:37:11 +0700

    chore: edit navigation name

--- src/IConnet.Presale.WebApp/Components/Pages/PageNavName.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/PageNavName.cs
@@ -6,7 +6,7 @@ public static class PageNavName
     public const string CrmImport = "Import CRM";
     public const string CrmVerification = "Verifikasi CRM";
     public const string WorkloadShared = "Kertas Kerja";
-    public const string ValidationStaging = "Kertas Kerja (H)";
+    public const string ValidationStaging = "Kertas Kerja (PH)";
     public const string Validation = "Validasi";
     public const string ApprovalStaging = "Kertas Kerja (PAC)";
     public const string Approval = "Approval";
