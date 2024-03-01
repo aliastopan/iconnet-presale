ab561a5f24ffc794495fa4f2861d088fc5b3487e
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-01 22:39:27 +0700

    add additional copy-and-update pattern `ApprovalProcess`

--- src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ApprovalProcess.cs
+++ src/IConnet.Presale.Domain/Aggregates/Presales/ValueObjects/ApprovalProcess.cs
@@ -32,6 +32,104 @@ public class ApprovalProcess : ValueObject
     [NotMapped]
     public bool IsOnGoing => SignatureApproval.IsEmptySignature();
 
+    public ApprovalProcess WithSignatureApproval(ActionSignature signatureApproval)
+    {
+        return new ApprovalProcess
+        {
+            SignatureApproval = signatureApproval,
+            StatusApproval = this.StatusApproval,
+            RootCause = this.RootCause,
+            Keterangan = this.Keterangan,
+            JarakShareLoc = this.JarakShareLoc,
+            JarakICrmPlus = this.JarakICrmPlus,
+            VaTerbit = this.VaTerbit
+        };
+    }
+
+    public ApprovalProcess WithSignatureApproval(ApprovalStatus approvalStatus)
+    {
+        return new ApprovalProcess
+        {
+            SignatureApproval = this.SignatureApproval,
+            StatusApproval = approvalStatus,
+            RootCause = this.RootCause,
+            Keterangan = this.Keterangan,
+            JarakShareLoc = this.JarakShareLoc,
+            JarakICrmPlus = this.JarakICrmPlus,
+            VaTerbit = this.VaTerbit
+        };
+    }
+
+    public ApprovalProcess WithRootCause(string rootCause)
+    {
+        return new ApprovalProcess
+        {
+            SignatureApproval = this.SignatureApproval,
+            StatusApproval = this.StatusApproval,
+            RootCause = rootCause,
+            Keterangan = this.Keterangan,
+            JarakShareLoc = this.JarakShareLoc,
+            JarakICrmPlus = this.JarakICrmPlus,
+            VaTerbit = this.VaTerbit
+        };
+    }
+
+    public ApprovalProcess WithKeterangan(string keterangan)
+    {
+        return new ApprovalProcess
+        {
+            SignatureApproval = this.SignatureApproval,
+            StatusApproval = this.StatusApproval,
+            RootCause = this.RootCause,
+            Keterangan = keterangan,
+            JarakShareLoc = this.JarakShareLoc,
+            JarakICrmPlus = this.JarakICrmPlus,
+            VaTerbit = this.VaTerbit
+        };
+    }
+
+    public ApprovalProcess WithJarakShareLoc(string jarakShareLoc)
+    {
+        return new ApprovalProcess
+        {
+            SignatureApproval = this.SignatureApproval,
+            StatusApproval = this.StatusApproval,
+            RootCause = this.RootCause,
+            Keterangan = this.Keterangan,
+            JarakShareLoc = jarakShareLoc,
+            JarakICrmPlus = this.JarakICrmPlus,
+            VaTerbit = this.VaTerbit
+        };
+    }
+
+    public ApprovalProcess WithJarakICrmPlus(string jarakShareLoc)
+    {
+        return new ApprovalProcess
+        {
+            SignatureApproval = this.SignatureApproval,
+            StatusApproval = this.StatusApproval,
+            RootCause = this.RootCause,
+            Keterangan = this.Keterangan,
+            JarakShareLoc = this.JarakShareLoc,
+            JarakICrmPlus = jarakShareLoc,
+            VaTerbit = this.VaTerbit
+        };
+    }
+
+    public ApprovalProcess WithVaTerbit(DateTime vaTerbit)
+    {
+        return new ApprovalProcess
+        {
+            SignatureApproval = this.SignatureApproval,
+            StatusApproval = this.StatusApproval,
+            RootCause = this.RootCause,
+            Keterangan = this.Keterangan,
+            JarakShareLoc = this.JarakShareLoc,
+            JarakICrmPlus = this.JarakICrmPlus,
+            VaTerbit = vaTerbit
+        };
+    }
+
     protected override IEnumerable<object> GetEqualityComponents()
     {
         yield return SignatureApproval;
