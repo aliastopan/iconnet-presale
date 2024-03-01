252d2d55450cdeb6860b543caa0cf261a3ad339a
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-01 14:22:45 +0700

    introduce `ApprovalStagingAlertDialog` component

---
+++ src/IConnet.Presale.WebApp/Components/Dialogs/ApprovalStagingAlertDialog.cs
@@ -0,0 +1,51 @@
+namespace IConnet.Presale.WebApp.Components.Dialogs;
+
+public partial class ApprovalStagingAlertDialog : IDialogContentComponent<WorkPaper>
+{
+    [Inject] public IDateTimeService DateTimeService { get; set; } = default!;
+    [Inject] public SessionService SessionService { get; set; } = default!;
+
+    [Parameter]
+    public WorkPaper Content { get; set; } = default!;
+
+    [CascadingParameter]
+    public FluentDialog Dialog { get; set; } = default!;
+
+    private async Task SaveAsync()
+    {
+        await RestageWorkPaperAsync();
+        await Dialog.CloseAsync(Content);
+    }
+
+    private async Task DeleteAsync()
+    {
+        UnstageWorkPaper();
+        await Dialog.CloseAsync(Content);
+    }
+
+    private async Task CancelAsync()
+    {
+        await Dialog.CancelAsync();
+    }
+
+    private async Task RestageWorkPaperAsync()
+    {
+        Content.Shift = (await SessionService.GetJobShiftAsync()).ToString();
+        Content.SetPlanningAssetCoverageInCharge(new ActionSignature
+        {
+            AccountIdSignature = await SessionService.GetUserAccountIdAsync(),
+            Alias = await SessionService.GetSessionAliasAsync(),
+            TglAksi = DateTimeService.DateTimeOffsetNow.DateTime
+        });
+    }
+
+    private void UnstageWorkPaper()
+    {
+        Content.SetPlanningAssetCoverageInCharge(new ActionSignature
+        {
+            AccountIdSignature = Guid.Empty,
+            Alias = string.Empty,
+            TglAksi = DateTimeService.Zero
+        });
+    }
+}
