380dca72561e6cc524e982730a249480a108a345
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-01 23:00:54 +0700

    refactor: `WorkPaperApprovalModel`

--- src/IConnet.Presale.WebApp/Models/Presales/WorkPaperApprovalModel.cs
+++ src/IConnet.Presale.WebApp/Models/Presales/WorkPaperApprovalModel.cs
@@ -5,7 +5,19 @@ public class WorkPaperApprovalModel
     public WorkPaperApprovalModel(WorkPaper workPaper)
     {
         IdPermohonan = workPaper.ApprovalOpportunity.IdPermohonan;
+
+        StatusApproval = EnumProcessor.EnumToDisplayString(workPaper.ProsesApproval.StatusApproval);
+        RootCause = workPaper.ProsesApproval.RootCause;
+        Keterangan = workPaper.ProsesApproval.Keterangan;
+        JarakShareLoc = workPaper.ProsesApproval.JarakShareLoc;
+        JarakICrmPlus = workPaper.ProsesApproval.JarakICrmPlus;
     }
 
     public string IdPermohonan { get; set; } = string.Empty;
+    public string StatusApproval { get; init; } = string.Empty;
+    public string RootCause { get; init; } = string.Empty;
+    public string Keterangan { get; init; } = string.Empty;
+    public string JarakShareLoc { get; init; } = string.Empty;
+    public string JarakICrmPlus { get; init; } = string.Empty;
+    public DateTime? NullableVaTerbit { get; init; } = DateTime.Today;
 }
