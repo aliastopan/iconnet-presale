303992497f29a61acd878a6d17ef5b8eef28b118
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-05 00:28:15 +0700

    feat: `ValidationBadge` with closed lost

--- src/IConnet.Presale.WebApp/Components/Custom/ValidationBadge.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/ValidationBadge.razor.cs
@@ -5,16 +5,36 @@ public partial class ValidationBadge : ComponentBase
     [Parameter]
     public ValidationStatus ValidationStatus { get; set; } = default!;
 
-    public string GetCssBadge()
+    [Parameter]
+    public bool IncludeClosedLost { get; set; } = false;
+
+    protected string GetCssBadge()
     {
         switch (ValidationStatus)
         {
+            case ValidationStatus.MenungguValidasi:
+                if(IncludeClosedLost)
+                    return "validation-badge-closed-lost";
+                else
+                    return "validation-badge-waiting";
             case ValidationStatus.TidakSesuai:
                 return "validation-badge-invalid";
             case ValidationStatus.Sesuai:
                 return "validation-badge-valid";
             default:
-                return "validation-badge-waiting";
+                return "validation-badge-neutral";
         }
     }
+
+    protected string GetValidationStatusString()
+    {
+        if (ValidationStatus == ValidationStatus.MenungguValidasi
+            && IncludeClosedLost)
+        {
+            return "Closed Lost";
+        }
+
+        return EnumProcessor.EnumToDisplayString(ValidationStatus);
+    }
+
 }
