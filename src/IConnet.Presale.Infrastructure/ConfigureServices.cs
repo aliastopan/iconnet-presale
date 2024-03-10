ad8b5e6085b00865656b260915bdb470d0c62584
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-10 10:51:12 +0700

    feat: enable MySql `EnableRetryOnFailure` method

--- src/IConnet.Presale.Infrastructure/ConfigureServices.cs
+++ src/IConnet.Presale.Infrastructure/ConfigureServices.cs
@@ -101,7 +101,13 @@ public static class ConfigureServices
 
             services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseMySql(connectionString, serverVersion);
+                options.UseMySql(connectionString, serverVersion, mySqlOptions =>
+                {
+                    mySqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                        errorNumbersToAdd: null);
+                });
                 options.EnableDetailedErrors();
             });
             services.AddScoped<AppDbContextFactory>();
