deec300d012492755af28e28c4ebcb8484d7b9ab
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-10 16:11:49 +0700

    drop configuration `TestEndpoint`

--- src/IConnet.Presale.Api/Endpoints/Common/TestEndpoint.cs
+++ src/IConnet.Presale.Api/Endpoints/Common/TestEndpoint.cs
@@ -5,24 +5,5 @@ public class TestEndpoint : IEndpointDefinition
     public void DefineEndpoints(WebApplication app)
     {
         app.MapGet("/", () => Result.Ok()).AllowAnonymous();
-        app.MapGet("/appsecrets", GetAppSecrets).AllowAnonymous();
-    }
-
-    internal IResult GetAppSecrets(IConfiguration configuration)
-    {
-        var appSecrets = new
-        {
-            MasterKey = configuration.GetValue<string>("AppSecrets:MasterKey"),
-            MySqlConnectionString = configuration.GetValue<string>("AppSecrets:MySqlConnectionString"),
-            RedisConnectionString = configuration.GetValue<string>("AppSecrets:RedisConnectionString"),
-            RedisPassword = configuration.GetValue<string>("AppSecrets:RedisPassword"),
-            RedisDbIndex = configuration.GetValue<string>("AppSecrets:RedisDbIndex"),
-            JwtIssuer = configuration.GetValue<string>("AppSecrets:JwtIssuer"),
-            JwtAudience = configuration.GetValue<string>("AppSecrets:JwtAudience"),
-            JwtLifeTime = configuration.GetValue<string>("AppSecrets:JwtLifeTime"),
-            JwtRefreshLifeTime = configuration.GetValue<string>("AppSecrets:JwtRefreshLifeTime")
-        };
-
-        return Results.Ok(appSecrets);
     }
 }
