72517e80274cf432b6774d5684b15e88af5616e2
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-11 19:31:27 +0700

    refactor: empty table content

--- src/IConnet.Presale.WebApp/Templates/HtmlFragment.cs
+++ src/IConnet.Presale.WebApp/Templates/HtmlFragment.cs
@@ -7,8 +7,9 @@ public static class HtmlFragment
     public static RenderFragment EmptyContent => builder =>
     {
         builder.OpenElement(0, "span");
-        builder.AddAttribute(1, "class", "");
-        builder.AddContent(2, "Data tidak tersedia.");
+        builder.AddAttribute(1, "class", "empty-table");
+        builder.AddAttribute(2, "style", "height: 320px");
+        builder.AddContent(3, "");
         builder.CloseElement();
     };
 
