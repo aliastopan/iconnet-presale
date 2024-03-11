c1ddd7dbf4daaf1cff4de269510f10cc355201c8
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-11 14:02:17 +0700

    introduce `WorkPaperExtensions`

---
+++ src/IConnet.Presale.Domain/Extensions/WorkPaperExtensions.cs
@@ -0,0 +1,12 @@
+using IConnet.Presale.Domain.Aggregates.Presales;
+using IConnet.Presale.Domain.Aggregates.Presales.ValueObjects;
+
+namespace IConnet.Presale.Domain.Extensions;
+
+public static class WorkPaperExtensions
+{
+    public static bool IsDoneProcessing(this WorkPaper workPaper)
+    {
+        return workPaper.WorkPaperLevel == WorkPaperLevel.DoneProcessing;
+    }
+}
