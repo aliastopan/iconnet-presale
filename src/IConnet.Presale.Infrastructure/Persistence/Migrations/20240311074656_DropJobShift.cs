701111e3e8e852f7cbd5df9aebed26c33c58a085
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-11 14:49:14 +0700

    migration: drop `JobShift`

---
+++ src/IConnet.Presale.Infrastructure/Persistence/Migrations/20240311074656_DropJobShift.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IConnet.Presale.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropJobShift : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "job_shift",
+                table: "dbo.user_account");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "job_shift",
+                table: "dbo.user_account",
+                type: "int",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Relational:ColumnOrder", 6);
+        }
+    }
+}
