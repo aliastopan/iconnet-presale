d7b0d726e9c922ff0d6dbe8a05c57ac5e3f57328
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-12 16:53:19 +0700

    add `WorkPaper` as parameter to `WorkPaperProgressTracker`

--- src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTracker.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTracker.razor.cs
@@ -1,6 +1,7 @@
 namespace IConnet.Presale.WebApp.Components.Custom.ProgressTrackers;
 
-public partial class WorkPaperProgressTracker
+public partial class WorkPaperProgressTracker : ComponentBase
 {
-
+    [Parameter]
+    public WorkPaper? WorkPaper { get; set; }
 }
