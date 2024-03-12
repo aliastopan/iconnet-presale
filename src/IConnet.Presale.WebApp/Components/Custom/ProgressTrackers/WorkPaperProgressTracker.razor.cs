591c57a3e0243a6a3c82c0279f3c1a5416f77ebc
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-12 17:43:43 +0700

    feat: dynamic step progress `Icon`

--- src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTracker.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTracker.razor.cs
@@ -4,4 +4,33 @@ public partial class WorkPaperProgressTracker : ComponentBase
 {
     [Parameter]
     public WorkPaper? WorkPaper { get; set; }
+
+    private readonly Icon _circleIcon = new Icons.Filled.Size20.Circle();
+    private readonly Icon _checkmarkIcon = new Icons.Filled.Size20.CheckmarkCircle();
+
+    // protected bool LessThanImportUnverified => WorkPaper?.WorkPaperLevel < WorkPaperLevel.ImportUnverified;
+    // protected bool EqualToImportUnverified => WorkPaper?.WorkPaperLevel == WorkPaperLevel.ImportUnverified;
+    // protected bool MoreThanImportUnverified => WorkPaper?.WorkPaperLevel > WorkPaperLevel.ImportUnverified;
+
+    protected bool ProceedImportUnverified => WorkPaper?.WorkPaperLevel >= WorkPaperLevel.ImportUnverified;
+
+    protected Icon GetImportStepIcon => GetStepIcon(WorkPaperLevel.ImportUnverified);
+
+    protected Icon GetStepIcon(WorkPaperLevel stepLevel)
+    {
+        if (WorkPaper?.WorkPaperLevel > stepLevel)
+        {
+            return _checkmarkIcon.WithColor("var(--accent-fill-rest)");
+        }
+        else if (WorkPaper?.WorkPaperLevel == stepLevel)
+        {
+            return _circleIcon.WithColor("var(--accent-fill-rest)");
+
+        }
+        else
+        {
+            return _circleIcon.WithColor("var(--info)");
+        }
+    }
+
 }
