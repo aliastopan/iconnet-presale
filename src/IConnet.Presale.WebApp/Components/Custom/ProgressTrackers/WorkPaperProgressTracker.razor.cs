1e4da9c476137da4cbe905a8f1a018ca71e56d60
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-13 04:10:26 +0700

    refactor: `WorkPaperProgressTracker` style format

--- src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTracker.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTracker.razor.cs
@@ -8,27 +8,27 @@ public partial class WorkPaperProgressTracker : ComponentBase
     private readonly string _greyedOut = "#e6e6e6";
 
     protected Icon ImportStepIcon => GetStepIcon(WorkPaperLevel.ImportUnverified);
-    protected string ImportStepHeaderColor => GetHeaderColor(WorkPaperLevel.ImportUnverified);
-    protected string ImportStepTrailColor => GetTrailColor(WorkPaperLevel.ImportUnverified);
+    protected string ImportStepHeaderColorStyle => GetHeaderColor(WorkPaperLevel.ImportUnverified);
+    protected string ImportStepTrailColorStyle => GetTrailColor(WorkPaperLevel.ImportUnverified);
     protected DateTime ImportDateTime => WorkPaper!.ApprovalOpportunity.SignatureImport.TglAksi;
     protected bool ProceedImportUnverified => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.ImportUnverified;
 
     protected Icon ImportVerifiedStepIcon => GetStepIcon(WorkPaperLevel.ImportVerified);
-    protected string ImportVerificationStepHeaderColor => GetHeaderColor(WorkPaperLevel.ImportVerified);
-    protected string ImportVerificationStepTrailColor => GetTrailColor(WorkPaperLevel.ImportVerified);
+    protected string ImportVerificationStepHeaderColorStyle => GetHeaderColor(WorkPaperLevel.ImportVerified);
+    protected string ImportVerificationStepTrailColorStyle => GetTrailColor(WorkPaperLevel.ImportVerified);
     protected DateTime ImportVerifiedDateTime => WorkPaper!.ApprovalOpportunity.SignatureVerifikasiImport.TglAksi;
     protected bool ProceedImportVerified => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.ImportVerified;
 
     protected Icon ValidatingStepIcon => GetStepIcon(WorkPaperLevel.Validating, checkOnGoing: true);
-    protected string ValidatingStepHeaderColor => GetHeaderColor(WorkPaperLevel.Validating);
-    protected string ValidatingStepTrailColor => GetTrailColor(WorkPaperLevel.Validating, checkOnGoing: true);
+    protected string ValidatingStepHeaderColorStyle => GetHeaderColor(WorkPaperLevel.Validating);
+    protected string ValidatingStepTrailColorStyle => GetTrailColor(WorkPaperLevel.Validating, checkOnGoing: true);
     protected DateTime ValidatingDateTime => WorkPaper!.ProsesValidasi.SignatureChatCallRespons.TglAksi;
     protected bool IsValidating => WorkPaper!.WorkPaperLevel == WorkPaperLevel.Validating;
     protected bool ProceedValidating => WorkPaper!.WorkPaperLevel > WorkPaperLevel.Validating;
 
     protected Icon WaitingApprovalStepIcon => GetStepIcon(WorkPaperLevel.WaitingApproval, checkOnGoing: true);
-    protected string WaitingApprovalStepHeaderColor => GetHeaderColor(WorkPaperLevel.WaitingApproval);
-    protected string WaitingApprovalStepTrailColor => GetTrailColor(WorkPaperLevel.WaitingApproval, checkOnGoing: true);
+    protected string WaitingApprovalStepHeaderColorStyle => GetHeaderColor(WorkPaperLevel.WaitingApproval);
+    protected string WaitingApprovalStepTrailColorStyle => GetTrailColor(WorkPaperLevel.WaitingApproval, checkOnGoing: true);
     protected DateTime ApprovalDateTime => WorkPaper!.ProsesApproval.SignatureApproval.TglAksi;
     protected bool IsWaitingApproval => WorkPaper!.WorkPaperLevel == WorkPaperLevel.WaitingApproval;
     protected bool ProceedWaitingApproval => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.WaitingApproval;
@@ -36,6 +36,12 @@ public partial class WorkPaperProgressTracker : ComponentBase
 
     protected Icon GetStepIcon(WorkPaperLevel stepLevel, bool checkOnGoing = false)
     {
+        if (WorkPaper?.WorkPaperLevel == WorkPaperLevel.DoneProcessing
+            && WorkPaper.ProsesApproval.StatusApproval != ApprovalStatus.Approve)
+        {
+            return new Icons.Filled.Size20.DismissCircle().WithColor("var(--error)");
+        }
+
         if (!checkOnGoing)
         {
             if (WorkPaper?.WorkPaperLevel >= stepLevel)
@@ -57,7 +63,7 @@ public partial class WorkPaperProgressTracker : ComponentBase
             {
                 if (WorkPaper?.WorkPaperLevel == stepLevel)
                 {
-                    return new Icons.Filled.Size20.Circle().WithColor("var(--accent-fill-rest)");
+                    return new Icons.Filled.Size20.QuestionCircle().WithColor("var(--info)");
 
                 }
 
@@ -68,6 +74,12 @@ public partial class WorkPaperProgressTracker : ComponentBase
 
     protected string GetTrailColor(WorkPaperLevel stepLevel, bool checkOnGoing = false)
     {
+        if (WorkPaper?.WorkPaperLevel == WorkPaperLevel.DoneProcessing
+            && WorkPaper.ProsesApproval.StatusApproval != ApprovalStatus.Approve)
+        {
+            return "border-left: 2px solid var(--error) !important;";
+        }
+
         if (!checkOnGoing)
         {
             if (WorkPaper?.WorkPaperLevel >= stepLevel)
@@ -89,7 +101,7 @@ public partial class WorkPaperProgressTracker : ComponentBase
             {
                 if (WorkPaper?.WorkPaperLevel == stepLevel)
                 {
-                    return "border-left: 2px solid var(--accent-fill-rest) !important;";
+                    return "border-left: 2px solid var(--info) !important;";
                 }
 
                 return $"border-left: 2px solid {_greyedOut} !important;";
@@ -107,5 +119,41 @@ public partial class WorkPaperProgressTracker : ComponentBase
         {
             return $"color: {_greyedOut};";
         }
+    }
+
+    protected Icon GetResultStepIcon()
+    {
+        if (WorkPaper?.ProsesApproval.StatusApproval == ApprovalStatus.Approve)
+        {
+            return new Icons.Filled.Size20.CheckmarkCircle().WithColor("var(--success)");
+        }
+        else
+        {
+            return new Icons.Filled.Size20.DismissCircle().WithColor("var(--error)");
+        }
+    }
+
+    protected string GetResultTrailColor()
+    {
+        if (WorkPaper?.ProsesApproval.StatusApproval == ApprovalStatus.Approve)
+        {
+            return "border-left: 2px solid var(--success) !important;";
+        }
+        else
+        {
+            return "border-left: 2px solid var(--error) !important;";
+        }
+    }
+
+    protected string GetApprovalStatus()
+    {
+        return WorkPaper!.ProsesApproval.StatusApproval.ToString();
+    }
+
+    protected string GetApprovalStatusLabelStyle()
+    {
+        return WorkPaper?.ProsesApproval.StatusApproval == ApprovalStatus.Approve
+            ? "background-color: var(--success); color: white;"
+            : "background-color: var(--error); color: white;";
     }
 }
