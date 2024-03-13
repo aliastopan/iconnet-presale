e131263d1ea6fa0af1e1ba618f113d6fc4acd07e
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-13 20:50:45 +0700

    introduce `WorkPaperLevelBadge`

---
+++ src/IConnet.Presale.WebApp/Components/Custom/Badges/WorkPaperLevelBadge.razor.cs
@@ -0,0 +1,53 @@
+namespace IConnet.Presale.WebApp.Components.Custom.Badges;
+
+public partial class WorkPaperLevelBadge : ComponentBase
+{
+    [Parameter]
+    public WorkPaperLevel WorkPaperLevel { get; set; } = default!;
+
+    protected string GetCssBadge()
+    {
+        switch (WorkPaperLevel)
+        {
+            case WorkPaperLevel.ImportUnverified:
+                return "workpaper-import-unverified";
+            case WorkPaperLevel.ImportInvalid:
+                return "workpaper-import-invalid";
+            case WorkPaperLevel.ImportArchived:
+                return "workpaper-import-archived";
+            case WorkPaperLevel.ImportVerified:
+                return "workpaper-import-verified";
+            case WorkPaperLevel.Validating:
+                return "workpaper-validating";
+            case WorkPaperLevel.WaitingApproval:
+                return "workpaper-waiting-approval";
+            case WorkPaperLevel.DoneProcessing:
+                return "workpaper-done-processing";
+            default:
+                return "workpaper-neutral";
+        }
+    }
+
+    protected string GetWorkPaperLevelString()
+    {
+        switch (WorkPaperLevel)
+        {
+            case WorkPaperLevel.ImportUnverified:
+                return "Menunggu Verifikasi";
+            case WorkPaperLevel.ImportInvalid:
+                return "Invalid iCRM+ Data";
+            case WorkPaperLevel.ImportArchived:
+                return "Archived";
+            case WorkPaperLevel.ImportVerified:
+                return "Menunggu Validasi";
+            case WorkPaperLevel.Validating:
+                return "Proses Validasi";
+            case WorkPaperLevel.WaitingApproval:
+                return "Menunggu Approval";
+            case WorkPaperLevel.DoneProcessing:
+                return "Selesai";
+            default:
+                return string.Empty;
+        }
+    }
+}
