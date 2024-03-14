91cdd6f4fb481e13961df3157093ede0ea862c39
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-14 22:38:11 +0700

    add CRM verification `InChargeDuration`

--- src/IConnet.Presale.WebApp/Helpers/InChargeDuration.cs
+++ src/IConnet.Presale.WebApp/Helpers/InChargeDuration.cs
@@ -2,6 +2,7 @@ namespace IConnet.Presale.WebApp.Helpers;
 
 public static class InChargeDuration
 {
+    public static readonly TimeSpan VerificationDuration = new TimeSpan(0, 10, 0);
     public static readonly TimeSpan ValidationDuration = new TimeSpan(0, 5, 0);
     public static readonly TimeSpan ApprovalDuration = new TimeSpan(0, 10, 0);
 }
