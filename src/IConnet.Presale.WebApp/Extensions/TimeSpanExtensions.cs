c9359e471e9ada48fbe4f4ce117edde23d94185c
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-14 21:11:37 +0700

    refactor: drop `AsReadableDateTimeWithSeconds` method extensions

--- src/IConnet.Presale.WebApp/Extensions/TimeSpanExtensions.cs
+++ src/IConnet.Presale.WebApp/Extensions/TimeSpanExtensions.cs
@@ -36,45 +36,4 @@ public static class TimeSpanExtensions
 
         return result;
     }
-
-    public static string AsReadableDateTimeWithSeconds(this TimeSpan timeSpan)
-    {
-        int days = timeSpan.Days;
-        int hours = timeSpan.Hours;
-        int minutes = timeSpan.Minutes;
-        int seconds = timeSpan.Seconds;
-
-        if (days >= 365)
-        {
-            return "Lebih dari 1 tahun";
-        }
-
-        string result = "";
-
-        if (days > 0)
-        {
-            result += $"{days} Hari ";
-        }
-
-        if (hours > 0)
-        {
-            result += $"{hours} Jam ";
-        }
-
-        if (minutes > 0)
-        {
-            result += $"{minutes} Menit ";
-        }
-
-        if (seconds > 0)
-        {
-            result += $"{seconds} Detik";
-        }
-        else
-        {
-            result = result.TrimEnd();
-        }
-
-        return result;
-    }
 }
