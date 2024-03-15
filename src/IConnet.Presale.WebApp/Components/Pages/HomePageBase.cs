87019b06ff69073ec92a3660c3a695033208992a
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-15 16:52:51 +0700

    feat (search): display single row of `WorkPaper`

--- src/IConnet.Presale.WebApp/Components/Pages/HomePageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/HomePageBase.cs
@@ -3,12 +3,20 @@ namespace IConnet.Presale.WebApp.Components.Pages;
 public class HomePageBase : ComponentBase, IPageNavigation
 {
     [Inject] public TabNavigationManager TabNavigationManager { get; set; } = default!;
+    [Inject] public IDateTimeService DateTimeService { get; set; } = default!;
     [Inject] public IWorkloadManager WorkloadManager { get; init; } = default!;
+
+    private IQueryable<WorkPaper>? _workPapers;
+    private readonly WorkloadColumnWidth _columnWidth = new WorkloadColumnWidth();
 
     public bool IsLoading { get; set; } = false;
     public bool HasSearched { get; set; } = false;
     public string IdPermohonan { get; set; } = default!;
     public WorkPaper? WorkPaper { get; set; }
+
+    protected string GridTemplateCols => GetGridTemplateCols();
+    protected IQueryable<WorkPaper>? WorkPapers => _workPapers;
+    protected WorkloadColumnWidth ColumnWidth => _columnWidth;
 
     public TabNavigationModel PageDeclaration()
     {
@@ -32,6 +40,21 @@ public class HomePageBase : ComponentBase, IPageNavigation
 
         await Task.CompletedTask;
 
+        if (WorkPaper is not null)
+        {
+            List<WorkPaper> result = [WorkPaper];
+
+            _workPapers = result.AsQueryable();
+            ColumnWidth.SetColumnWidth(_workPapers);
+        }
+        else
+        {
+            List<WorkPaper> result = [];
+
+            _workPapers = result.AsQueryable();
+            HasSearched = false;
+        }
+
         IsLoading = false;
     }
 
@@ -43,5 +66,55 @@ public class HomePageBase : ComponentBase, IPageNavigation
         }
 
         return WorkPaper.ApprovalOpportunity.SignatureImport.TglAksi.Date.ToString();
+    }
+
+    protected string GetWidthStyle(int widthPx, int offsetPx = 0)
+    {
+        return $"width: {widthPx + offsetPx}px;";
+    }
+
+    private string GetGridTemplateCols()
+    {
+        return $@"
+            {ColumnWidth.WorkPaperLevelPx + ColumnWidth.OffsetPx}px
+            {ColumnWidth.IdPermohonanPx + ColumnWidth.OffsetPx}px
+            {ColumnWidth.TglPermohonanPx}px
+            {ColumnWidth.DurasiTidakLanjutPx}px
+            {ColumnWidth.NamaPemohonPx + 200}px
+            {ColumnWidth.TelpPemohonPx + 180}px
+            {ColumnWidth.EmailPemohonPx + 200}px
+            {ColumnWidth.IdPlnPx + 180}px
+            {ColumnWidth.AlamatPemohonPx + 200}px
+            {ColumnWidth.ValidasiShareLocPx}px
+            {ColumnWidth.NikPemohonPx}px
+            {ColumnWidth.NpwpPemohonPx}px
+            {ColumnWidth.KeteranganPx}px
+            {ColumnWidth.TglChatCallMulaiPx}px
+            {ColumnWidth.TglChatCallResponsPx}px
+            {ColumnWidth.TglChatCallMulaiPx}px
+            {ColumnWidth.LinkChatHistoryPx}px
+            {ColumnWidth.KeteranganValidasiPx}px
+            {ColumnWidth.ContactWhatsAppPx}px
+            {ColumnWidth.StatusApprovalPx}px
+            {ColumnWidth.RootCausePx}px
+            {ColumnWidth.TglApprovalPx}px
+            {ColumnWidth.PlanningAssetCoverageInChargePx}px
+            {ColumnWidth.KeteranganApprovalPx}px
+            {ColumnWidth.LayananPx}px
+            {ColumnWidth.SumberPermohonanPx}px
+            {ColumnWidth.StatusPermohonanPx}px
+            {ColumnWidth.NamaAgenPx}px
+            {ColumnWidth.EmailAgenPx}px
+            {ColumnWidth.TelpAgenPx}px
+            {ColumnWidth.MitraAgenPx}px
+            {ColumnWidth.SplitterPx}px
+            {ColumnWidth.RegionalPx}px
+            {ColumnWidth.KantorPerwakilanPx}px
+            {ColumnWidth.ProvinsiPx}px
+            {ColumnWidth.KabupatenPx}px
+            {ColumnWidth.KecamatanPx}px
+            {ColumnWidth.KelurahanPx}px
+            {ColumnWidth.LatitudePx}px
+            {ColumnWidth.LongitudePx}px;";
     }
 }
