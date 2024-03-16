527597e74a0302445a5ce2001ed6a9aaa4e1d585
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-16 18:41:15 +0700

    add `TransformWorkPaperFromModel` method

--- src/IConnet.Presale.Infrastructure/Factories/WorkPaperFactory.cs
+++ src/IConnet.Presale.Infrastructure/Factories/WorkPaperFactory.cs
@@ -147,4 +147,130 @@ internal sealed class WorkPaperFactory
             LastModified = _dateTimeService.DateTimeOffsetNow
         };
     }
+
+    internal WorkPaper TransformWorkPaperFromModel(IWorkPaperModel model)
+    {
+        return new WorkPaper
+        {
+            WorkPaperId = model.WorkPaperId,
+            WorkPaperLevel = (WorkPaperLevel)model.WorkPaperLevel,
+            Shift = model.Shift,
+            SignatureHelpdeskInCharge = new ActionSignature
+            {
+                AccountIdSignature = model.HelpdeskInChargeAccountIdSignature,
+                Alias = model.HelpdeskInChargeAliasSignature,
+                TglAksi = model.HelpdeskInChargeTglAksiSignature
+            },
+            SignaturePlanningAssetCoverageInCharge = new ActionSignature
+            {
+                AccountIdSignature = model.PlanningAssetCoverageInChargeAccountIdSignature,
+                Alias = model.PlanningAssetCoverageInChargeAliasSignature,
+                TglAksi = model.PlanningAssetCoverageInChargeTglAksiSignature
+            },
+            ProsesValidasi = new ValidationProcess
+            {
+                SignatureChatCallMulai = new ActionSignature
+                {
+                    AccountIdSignature = model.ChatCallMulaiAccountIdSignature,
+                    Alias = model.ChatCallMulaiAliasSignature,
+                    TglAksi = model.ChatCallMulaiTglAksiSignature
+                },
+                SignatureChatCallRespons = new ActionSignature
+                {
+                    AccountIdSignature = model.ChatCallResponsAccountIdSignature,
+                    Alias = model.ChatCallResponsAliasSignature,
+                    TglAksi = model.ChatCallResponsTglAksiSignature
+                },
+                WaktuTanggalRespons = model.WaktuTanggalRespons,
+                LinkChatHistory = model.LinkChatHistory,
+                ParameterValidasi = new ValidationParameter
+                {
+                    ValidasiIdPln = (ValidationStatus)model.ValidasiIdPln,
+                    ValidasiNama = (ValidationStatus)model.ValidasiNama,
+                    ValidasiNomorTelepon = (ValidationStatus)model.ValidasiNomorTelepon,
+                    ValidasiEmail = (ValidationStatus)model.ValidasiEmail,
+                    ValidasiAlamat = (ValidationStatus)model.ValidasiAlamat,
+                    ShareLoc = new Coordinate
+                    {
+                        Latitude = model.ShareLocLatitude,
+                        Longitude = model.ShareLocLongitude
+                    }
+                },
+                Keterangan = model.KeteranganValidasi
+            },
+            ProsesApproval = new ApprovalProcess
+            {
+                SignatureApproval = new ActionSignature
+                {
+                    AccountIdSignature = model.ApprovalAccountIdSignature,
+                    Alias = model.ApprovalAliasSignature,
+                    TglAksi = model.ApprovalTglAksiSignature
+                },
+                StatusApproval = (ApprovalStatus)model.StatusApproval,
+                RootCause = model.RootCause,
+                Keterangan = model.KeteranganApproval,
+                JarakShareLoc = model.JarakShareLoc,
+                JarakICrmPlus = model.JarakICrmPlus,
+                VaTerbit = model.VaTerbit
+            },
+            FkApprovalOpportunityId = model.ApprovalOpportunityId,
+            ApprovalOpportunity = new ApprovalOpportunity
+            {
+                IdPermohonan = model.IdPermohonan,
+                TglPermohonan = model.TglPermohonan,
+                StatusPermohonan = model.StatusPermohonan,
+                Layanan = model.Layanan,
+                SumberPermohonan = model.SumberPermohonan,
+                JenisPermohonan = model.JenisPermohonan,
+                Splitter = model.Splitter,
+                Pemohon = new Applicant
+                {
+                    IdPln = model.IdPlnPelanggan,
+                    NamaPelanggan = model.NamaPelanggan,
+                    NomorTelepon = model.NomorTeleponPelanggan,
+                    Email = model.EmailPelanggan,
+                    Alamat = model.AlamatPelanggan,
+                    Nik = model.NikPelanggan,
+                    Npwp = model.NpwpPelanggan,
+                    Keterangan = model.KeteranganPelanggan
+                },
+                Agen = new Salesperson
+                {
+                    NamaLengkap = model.NamaAgen,
+                    Email = model.EmailAgen,
+                    NomorTelepon = model.NomorTeleponAgen,
+                    Mitra = model.MitraAgen
+                },
+                Regional = new Regional
+                {
+                    Bagian = model.Bagian,
+                    KantorPerwakilan = model.KantorPerwakilan,
+                    Provinsi = model.Provinsi,
+                    Kabupaten = model.Kabupaten,
+                    Kecamatan = model.Kecamatan,
+                    Kelurahan = model.Kelurahan,
+                    Koordinat = new Coordinate
+                    {
+                        Latitude = model.KoordinatLatitude,
+                        Longitude = model.KoordinatLongitude
+                    }
+                },
+                StatusImport = (ImportStatus)model.StatusImport,
+                SignatureImport = new ActionSignature
+                {
+                    AccountIdSignature = model.ImportAccountIdSignature,
+                    Alias = model.ImportAliasSignature,
+                    TglAksi = model.ImportTglAksiSignature,
+
+                },
+                SignatureVerifikasiImport = new ActionSignature
+                {
+                    AccountIdSignature = model.VerifikasiImportAccountIdSignature,
+                    Alias = model.VerifikasiImportAliasSignature,
+                    TglAksi = model.VerifikasiImportTglAksiSignature
+                }
+            },
+            LastModified = _dateTimeService.DateTimeOffsetNow
+        };
+    }
 }
