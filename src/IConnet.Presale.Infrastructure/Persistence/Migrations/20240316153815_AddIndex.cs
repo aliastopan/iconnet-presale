d68e814e689d15d5786c7c663110d11cb63c3a73
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-16 22:43:03 +0700

    migration: `IdPermohonan` as index

---
+++ src/IConnet.Presale.Infrastructure/Persistence/Migrations/20240316153815_AddIndex.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IConnet.Presale.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "id_permohonan",
+                table: "dbo.approval_opportunity",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_id_permohonan",
+                table: "dbo.approval_opportunity",
+                column: "id_permohonan");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_id_permohonan",
+                table: "dbo.approval_opportunity");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "id_permohonan",
+                table: "dbo.approval_opportunity",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
