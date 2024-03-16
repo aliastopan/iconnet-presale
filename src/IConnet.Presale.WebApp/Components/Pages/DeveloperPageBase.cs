3d312e5aa5fb02aa1acbe5e5f2e6a03ae031c198
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-16 23:00:51 +0700

    mock: prep `DeveloperPageBase`

--- src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
@@ -1,6 +1,13 @@
 namespace IConnet.Presale.WebApp.Components.Pages;
 
-public class DeveloperPageBase : ComponentBase
+public class DeveloperPageBase : IndexPageBase
 {
+    [Inject] public IWorkPaperHttpClient WorkPaperHttpClient { get; set; } = default!;
 
+    protected async Task ForwardingAsync()
+    {
+        await Task.CompletedTask;
+
+        LogSwitch.Debug("Forwarding...");
+    }
 }
