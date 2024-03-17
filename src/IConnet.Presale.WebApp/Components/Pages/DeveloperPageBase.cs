b158e68da0a2493cb247a671410655200de88ab8
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-17 13:28:38 +0700

    feat: SQL forwarding

--- src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
@@ -1,3 +1,8 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using IConnet.Presale.WebApp.Models.Presales;
+using Mapster;
+
 namespace IConnet.Presale.WebApp.Components.Pages;
 
 public class DeveloperPageBase : IndexPageBase
@@ -6,8 +11,38 @@ public class DeveloperPageBase : IndexPageBase
 
     protected async Task ForwardingAsync()
     {
-        await Task.CompletedTask;
+        if (WorkPaper is null)
+        {
+            LogSwitch.Debug("Not found.");
+            return;
+        }
+
+        if (WorkPaper.WorkPaperLevel < WorkPaperLevel.DoneProcessing)
+        {
+            LogSwitch.Debug("Not done yet.");
+            return;
+        }
+
+        var jsonModel = new WorkPaperFlatJsonModel(WorkPaper);
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
 
         LogSwitch.Debug("Forwarding...");
+
+        var httpResult = await WorkPaperHttpClient.InsertWorkPaperAsync(jsonModel);
+
+        if (httpResult.IsSuccessStatusCode)
+        {
+            LogSwitch.Debug("Success: {0}", httpResult.Content);
+        }
+        else
+        {
+            var problemDetails = JsonSerializer.Deserialize<ProblemDetails>(httpResult.Content, options);
+            var extension = problemDetails.GetProblemDetailsExtension();
+
+            LogSwitch.Debug("Error {message}: ", extension.Errors.First().Message);
+        }
     }
 }
