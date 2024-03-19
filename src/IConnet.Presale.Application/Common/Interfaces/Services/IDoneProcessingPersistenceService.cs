cba7e79b3da85df7baacffa5a74d722ac6f542a6
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-19 18:46:20 +0700

    add existing keys check

--- src/IConnet.Presale.Application/Common/Interfaces/Services/IDoneProcessingPersistenceService.cs
+++ src/IConnet.Presale.Application/Common/Interfaces/Services/IDoneProcessingPersistenceService.cs
@@ -3,4 +3,6 @@ namespace IConnet.Presale.Application.Common.Interfaces.Services;
 public interface IDoneProcessingPersistenceService
 {
     Task ArchiveValueAsync(string key, string value, TimeSpan? expiry = null);
+    Task<bool> IsKeyExistsAsync(string key);
+    Task<HashSet<string>> GetExistingKeysAsync(HashSet<string> keysToCheck);
 }
