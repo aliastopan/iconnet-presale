5bf6c45c75ef2f040b6e09dd94afa264305025aa
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-19 21:52:26 +0700

    feat: auto scroll-up `ChatTemplateDialog`

--- src/IConnet.Presale.WebApp/Components/Dialogs/ChatTemplateDialog.cs
+++ src/IConnet.Presale.WebApp/Components/Dialogs/ChatTemplateDialog.cs
@@ -1,9 +1,10 @@
 namespace IConnet.Presale.WebApp.Components.Dialogs;
 
-public partial class ChatTemplateDialog : IDialogContentComponent<WorkPaper>
+public partial class ChatTemplateDialog : ComponentBase, IDialogContentComponent<WorkPaper>
 {
     [Inject] public ChatTemplateService ChatTemplateService { get; set; } = default!;
     [Inject] public IDateTimeService DateTimeService { get; set; } = default!;
+    [Inject] public IJSRuntime JsRuntime { get; set; } = default!;
     [Inject] public SessionService SessionService { get; set; } = default!;
 
     [Parameter]
@@ -15,6 +16,17 @@ public partial class ChatTemplateDialog : IDialogContentComponent<WorkPaper>
     protected override void OnInitialized()
     {
         base.OnInitialized();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            var elementId = "chat-template";
+            await JsRuntime.InvokeVoidAsync("scrollToElement", elementId);
+
+            LogSwitch.Debug("auto-scroll");
+        }
     }
 
     private async Task CancelAsync()
