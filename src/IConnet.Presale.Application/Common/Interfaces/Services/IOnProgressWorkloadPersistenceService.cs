6877e82f78baa4ae3fca1645c6d147ac00a4d76e
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-19 16:19:09 +0700

    introduce `IOnProgressWorkloadPersistence` service interface

---
+++ src/IConnet.Presale.Application/Common/Interfaces/Services/IOnProgressWorkloadPersistenceService.cs
@@ -0,0 +1,14 @@
+namespace IConnet.Presale.Application.Common.Interfaces.Services;
+
+public interface IOnProgressWorkloadPersistenceService
+{
+    Task<string?> GetValueAsync(string key);
+    Task<List<string?>> GetAllValuesAsync();
+    Task SetValueAsync(string key, string value, TimeSpan? expiry = null);
+    Task<bool> UpdateValueAsync(string key, string value, TimeSpan? expiry = null);
+    Task<bool> DeleteValueAsync(string key);
+    Task<bool> IsKeyExistsAsync(string key);
+    Task<HashSet<string>> GetExistingKeysAsync(HashSet<string> keysToCheck);
+
+    Task SetBackupValueAsync(string key, string value, TimeSpan? expiry = null);
+}
