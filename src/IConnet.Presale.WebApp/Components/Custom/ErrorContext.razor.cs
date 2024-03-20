0143f370ccfae5e1d585bbc357a072ac17bb6359
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-20 21:34:29 +0700

    feat: error recovery

--- src/IConnet.Presale.WebApp/Components/Custom/ErrorContext.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/ErrorContext.razor.cs
@@ -2,5 +2,12 @@ namespace IConnet.Presale.WebApp.Components.Custom;
 
 public partial class ErrorContext : ComponentBase
 {
+    [Inject] public NavigationManager NavigationManager { get; set; } = default!;
+
     [Parameter] public Exception Exception { get; set; } = default!;
+
+    protected void ReloadCurrentPage()
+    {
+        NavigationManager.NavigateTo(NavigationManager.Uri, forceLoad: true);
+    }
 }
