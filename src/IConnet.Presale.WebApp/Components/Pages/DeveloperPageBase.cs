c786ee2c49c30541602fef84a67376c73d490cca
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-20 23:24:54 +0700

    mock: custom `RootCause` option form

--- src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
@@ -8,6 +8,42 @@ namespace IConnet.Presale.WebApp.Components.Pages;
 public class DeveloperPageBase : IndexPageBase
 {
     [Inject] public IWorkPaperHttpClient WorkPaperHttpClient { get; set; } = default!;
+    [Inject] public OptionService OptionService { get; set; } = default!;
+
+    private ICollection<string> _rootCauses = default!;
+
+    protected string SelectedRootCause = string.Empty;
+    protected string RootCauseFilter { get; set; } = string.Empty;
+    protected IEnumerable<string> RootCauses => GetFilteredRootCauses();
+    protected int DropdownHeight => Math.Min(RootCauses.Count() * 40, 200);
+    protected string DropdownHeightPx => $"{DropdownHeight}px";
+    protected bool IsPopoverVisible { get; set; }
+    protected bool FirstClick { get; set; }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        FirstClick = true;
+        IsPopoverVisible = true;
+
+        _rootCauses = new List<string>(OptionService.RootCauseOptions);
+    }
+
+    protected void OpenPopover()
+    {
+        LogSwitch.Debug("before:{0}", IsPopoverVisible);
+        // if (FirstClick)
+        // {
+        //     IsPopoverVisible = true;
+        //     FirstClick = false;
+        // }
+        // else
+        // {
+        // }
+        IsPopoverVisible = !IsPopoverVisible;
+        LogSwitch.Debug("after:{0}", IsPopoverVisible);
+    }
 
     protected async Task ForwardingAsync()
     {
@@ -51,6 +87,32 @@ public class DeveloperPageBase : IndexPageBase
             var extension = problemDetails.GetProblemDetailsExtension();
 
             LogSwitch.Debug("Error {message}: ", extension.Errors.First().Message);
+        }
+    }
+
+    // protected async Task OnSearchAsync(OptionsSearchEventArgs<string> eventArg)
+    // {
+    //     await Task.CompletedTask;
+
+    //     var rootCauses = OptionService.RootCauseOptions;
+    //     eventArg.Items = rootCauses.Where(option => option.StartsWith(eventArg.Text, StringComparison.OrdinalIgnoreCase));
+    // }
+
+    protected void OnRootCauseChanged(string rootCauses)
+    {
+        IsPopoverVisible = false;
+        SelectedRootCause = rootCauses;
+    }
+
+    protected IEnumerable<string> GetFilteredRootCauses()
+    {
+        if (RootCauseFilter.IsNullOrWhiteSpace())
+        {
+            return _rootCauses;
+        }
+        else
+        {
+            return _rootCauses.Where(option => option.StartsWith(RootCauseFilter, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
