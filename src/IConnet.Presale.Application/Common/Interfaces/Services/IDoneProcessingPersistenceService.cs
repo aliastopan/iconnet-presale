f0dbf76a6d7fc522f1ce919dc349d3e8abc0d135
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-21 15:09:08 +0700

    add `GetAllValues` async method

--- src/IConnet.Presale.Application/Common/Interfaces/Services/IDoneProcessingPersistenceService.cs
+++ src/IConnet.Presale.Application/Common/Interfaces/Services/IDoneProcessingPersistenceService.cs
@@ -3,6 +3,7 @@ namespace IConnet.Presale.Application.Common.Interfaces.Services;
 public interface IDoneProcessingPersistenceService
 {
     Task<string?> GetValueAsync(string key);
+    Task<List<string?>> GetAllValuesAsync();
     Task ArchiveValueAsync(string key, string value, TimeSpan? expiry = null);
     Task<bool> IsKeyExistsAsync(string key);
     Task<HashSet<string>> GetExistingKeysAsync(HashSet<string> keysToCheck);
