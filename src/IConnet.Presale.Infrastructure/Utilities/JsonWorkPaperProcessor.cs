29c978d30be44315c3d02591d526a78d2529384e
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-21 17:50:31 +0700

    add `DeserializeJsonWorkPapersParallel` method

--- src/IConnet.Presale.Infrastructure/Utilities/JsonWorkPaperProcessor.cs
+++ src/IConnet.Presale.Infrastructure/Utilities/JsonWorkPaperProcessor.cs
@@ -65,4 +65,34 @@ internal static class JsonWorkPaperProcessor
 
         return concurrentBag.ToList();
     }
+
+    internal static IEnumerable<WorkPaper> DeserializeJsonWorkPapersParallel(IEnumerable<string> jsonWorkPapers,
+        ParallelOptions parallelOptions, Func<WorkPaper, bool>? filterPredicate = null)
+    {
+        var concurrentBag = new ConcurrentBag<WorkPaper>();
+
+        Parallel.ForEach(jsonWorkPapers, parallelOptions, json =>
+        {
+            try
+            {
+                if (json is null)
+                {
+                    throw new JsonException();
+                }
+
+                var workPaper = JsonSerializer.Deserialize<WorkPaper>(json);
+
+                if (filterPredicate == null || filterPredicate(workPaper!))
+                {
+                    concurrentBag.Add(workPaper!);
+                }
+            }
+            catch (JsonException exception)
+            {
+                Log.Fatal("Error deserializing JSON: {message}", exception.Message);
+            }
+        });
+
+        return concurrentBag.ToList();
+    }
 }
