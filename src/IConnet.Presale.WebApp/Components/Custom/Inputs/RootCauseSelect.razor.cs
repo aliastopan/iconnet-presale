3403577d2cc1b2fedcb53ccc68fe144a83cc2df2
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-21 13:18:30 +0700

    refactor: adjust `DropdownHeight`

--- src/IConnet.Presale.WebApp/Components/Custom/Inputs/RootCauseSelect.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/Inputs/RootCauseSelect.razor.cs
@@ -5,6 +5,8 @@ public partial class RootCauseSelect : ComponentBase
     [Inject] public OptionService OptionService { get; set; } = default!;
 
     private ICollection<string> _rootCauses = default!;
+    private readonly int _maxHeight = 40;
+    private readonly int _maxSelectionHeight = 320 + 6; // with offset hack
 
     [Parameter]
     public string SelectedRootCause { get; set; } = string.Empty;
@@ -17,7 +19,7 @@ public partial class RootCauseSelect : ComponentBase
 
     protected string RootCauseFilter { get; set; } = string.Empty;
     protected IEnumerable<string> RootCauses => GetFilteredRootCauses();
-    protected int DropdownHeight => Math.Min(RootCauses.Count() * 40, 200);
+    protected int DropdownHeight => Math.Min(RootCauses.Count() * _maxHeight, _maxSelectionHeight);
     protected string DropdownHeightPx => $"{DropdownHeight}px";
     protected bool IsPopoverVisible { get; set; }
 
