e4ded9ba37e3045db724223d9a89b1aba7e32010
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-22 23:25:52 +0700

    mock: fetch `UserOperator`

--- src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
@@ -1,13 +1,21 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using IConnet.Presale.WebApp.Models.Identity;
+using IConnet.Presale.Shared.Contracts.Identity;
+
 namespace IConnet.Presale.WebApp.Components.Pages;
 
 public class DeveloperPageBase : ComponentBase
 {
     [Inject] IDashboardManager DashboardManager { get; set; } = default!;
+    [Inject] IIdentityHttpClient IdentityHttpClient { get; set; } = default!;
 
     private bool _isInitialized = false;
+    private readonly List<UserOperatorModel> _userOperatorModels = [];
     private IQueryable<WorkPaper>? _presaleData;
 
     protected IQueryable<WorkPaper>? PresaleData => _presaleData;
+    protected List<UserOperatorModel> UserOperatorModels => _userOperatorModels;
 
     protected override async Task OnInitializedAsync()
     {
@@ -15,7 +23,36 @@ public class DeveloperPageBase : ComponentBase
         {
             _presaleData = await DashboardManager.GetPresaleDataFromCurrentMonthAsync();
 
+            await GetUserOperators();
+
             _isInitialized = true;
+        }
+    }
+
+    private async Task GetUserOperators()
+    {
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var httpResult = await IdentityHttpClient.GetUserOperatorsAsync();
+
+        if (httpResult.IsSuccessStatusCode)
+        {
+            var response = JsonSerializer.Deserialize<GetUserOperatorsQueryResponse>(httpResult.Content, options);
+            var userOperatorDtos = response!.UserOperatorDtos;
+
+            foreach (var dto in userOperatorDtos)
+            {
+                _userOperatorModels.Add(new UserOperatorModel(dto));
+            }
+        }
+        else
+        {
+            var problemDetails = JsonSerializer.Deserialize<ProblemDetails>(httpResult.Content, options);
+            var extension = problemDetails.GetProblemDetailsExtension();
+            LogSwitch.Debug("Error: {0}", extension.Errors.First().Message);
         }
     }
 }
