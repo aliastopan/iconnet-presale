7aaf7979ac1e9bc429de644641597495a37e659b
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-22 21:45:34 +0700

    mock: dashboard in developer page

--- src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
@@ -1,6 +1,21 @@
 namespace IConnet.Presale.WebApp.Components.Pages;
 
-public class DeveloperPageBase : IndexPageBase
+public class DeveloperPageBase : ComponentBase
 {
+    [Inject] IDashboardManager DashboardManager { get; set; } = default!;
 
+    private bool _isInitialized = false;
+    private IQueryable<WorkPaper>? _presaleData;
+
+    protected IQueryable<WorkPaper>? PresaleData => _presaleData;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (!_isInitialized)
+        {
+            _presaleData = await DashboardManager.GetPresaleDataFromCurrentMonthAsync();
+
+            _isInitialized = true;
+        }
+    }
 }
