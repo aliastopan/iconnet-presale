f3e1116395a27f79447c08c9262fe4ca97af4c0b
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-22 21:16:46 +0700

    introduce `GetPresaleDataFromCurrentMonth` async method

--- src/IConnet.Presale.Application/Common/Interfaces/Managers/IDashboardManager.cs
+++ src/IConnet.Presale.Application/Common/Interfaces/Managers/IDashboardManager.cs
@@ -1,6 +1,8 @@
+using IConnet.Presale.Domain.Aggregates.Presales;
+
 namespace IConnet.Presale.Application.Common.Interfaces.Managers;
 
 public interface IDashboardManager
 {
-
+    Task<IQueryable<WorkPaper>> GetPresaleDataFromCurrentMonthAsync();
 }
--- src/IConnet.Presale.Infrastructure/Managers/DashboardManager.cs
+++ src/IConnet.Presale.Infrastructure/Managers/DashboardManager.cs
@@ -1,6 +1,58 @@
+using IConnet.Presale.Domain.Aggregates.Presales;
+
 namespace IConnet.Presale.Infrastructure.Managers;
 
-internal sealed class DashboardManager : IDashboardManager
+internal sealed class DashboardManager : PresaleDataOperationBase, IDashboardManager
 {
+    private readonly IDateTimeService _dateTimeService;
+    private readonly IInProgressPersistenceService _inProgressPersistenceService;
+    private readonly IDoneProcessingPersistenceService _doneProcessingPersistenceService;
+
+    public DashboardManager(IDateTimeService dateTimeService,
+        IInProgressPersistenceService inProgressPersistenceService,
+        IDoneProcessingPersistenceService doneProcessingPersistenceService)
+    {
+        _dateTimeService = dateTimeService;
+        _inProgressPersistenceService = inProgressPersistenceService;
+        _doneProcessingPersistenceService = doneProcessingPersistenceService;
+    }
+
+    public async Task<IQueryable<WorkPaper>> GetPresaleDataFromCurrentMonthAsync()
+    {
+        Task<List<string?>>[] getJsonWorkPapers =
+        [
+            _inProgressPersistenceService.GetAllValuesAsync(),
+            _doneProcessingPersistenceService.GetAllValuesAsync()
+        ];
+
+        await Task.WhenAll(getJsonWorkPapers);
+
+        var inProgressJsonWorkPapers = getJsonWorkPapers[0].Result;
+        var doneProcessingJsonWorkPaper = getJsonWorkPapers[1].Result;
+
+        Task<List<WorkPaper>>[] workPaperTasks =
+        {
+            Task.Run(() => ProcessJsonWorkPapers(inProgressJsonWorkPapers!)),
+            Task.Run(() => ProcessJsonWorkPapers(doneProcessingJsonWorkPaper!))
+        };
+
+        await Task.WhenAll(workPaperTasks);
+
+        List<WorkPaper> inProgressWorkPapers = workPaperTasks[0].Result;
+        List<WorkPaper> doneProcessingWorkPapers = workPaperTasks[1].Result;
+
+        return doneProcessingWorkPapers.Concat(inProgressWorkPapers).AsQueryable();
+
+        // local function
+        List<WorkPaper> ProcessJsonWorkPapers(List<string> jsonWorkPapers)
+        {
+            var currentMonth = _dateTimeService.DateTimeOffsetNow.Month;
+            return JsonWorkPaperProcessor.DeserializeJsonWorkPapersParallel(jsonWorkPapers, ParallelOptions,
+                workPaper =>
+                {
+                    return workPaper.ApprovalOpportunity.TglPermohonan.Month == currentMonth;
+                });
+        }
+    }
 
 }
