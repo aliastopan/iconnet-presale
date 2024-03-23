86c80aeceae65021dfab6f31fdba5044078d22d9
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-23 18:37:27 +0700

    refactor (major): change `UserRole` value

--- src/IConnet.Presale.Domain/Enums/UserRole.cs
+++ src/IConnet.Presale.Domain/Enums/UserRole.cs
@@ -4,8 +4,17 @@ namespace IConnet.Presale.Domain.Enums;
 public enum UserRole
 {
     PTL = 0,
-    Helpdesk = 1,
-    PAC = 2,
-    Administrator = 4,
+    // Sales,
+    Helpdesk = 8,
+    PAC = 16,
+    // Management
+    Administrator = 32,
     SuperUser = 64
 }
+
+// original value
+// PTL = 0,
+// Helpdesk = 1,
+// PAC = 2,
+// Administrator = 4,
+// SuperUser = 64
