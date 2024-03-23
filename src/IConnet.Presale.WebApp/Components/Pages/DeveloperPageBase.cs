de9f2c6f3cb4fe488c1e4d990a2ac392996a0a84
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-23 11:48:57 +0700

    mock: rough draft `Dashboard`

--- src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
@@ -2,20 +2,26 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using IConnet.Presale.WebApp.Models.Identity;
 using IConnet.Presale.Shared.Contracts.Identity;
+using IConnet.Presale.WebApp.Models.Presales;
 
 namespace IConnet.Presale.WebApp.Components.Pages;
 
 public class DeveloperPageBase : ComponentBase
 {
-    [Inject] IDashboardManager DashboardManager { get; set; } = default!;
-    [Inject] IIdentityHttpClient IdentityHttpClient { get; set; } = default!;
+    [Inject] protected IDashboardManager DashboardManager { get; set; } = default!;
+    [Inject] protected IIdentityHttpClient IdentityHttpClient { get; set; } = default!;
+    [Inject] protected OptionService OptionService { get; set; } = default!;
+    [Inject] protected ReportService ReportService { get; set; } = default!;
 
     private bool _isInitialized = false;
     private readonly List<UserOperatorModel> _userOperatorModels = [];
     private IQueryable<WorkPaper>? _presaleData;
+    private List<ApprovalStatusReportModel> _approvalStatusReportModels = [];
 
-    protected IQueryable<WorkPaper>? PresaleData => _presaleData;
-    protected List<UserOperatorModel> UserOperatorModels => _userOperatorModels;
+
+    public List<UserOperatorModel> UserOperatorModels => _userOperatorModels;
+    public IQueryable<WorkPaper>? PresaleData => _presaleData;
+    public List<ApprovalStatusReportModel> ApprovalStatusReportModels => _approvalStatusReportModels;
 
     protected override async Task OnInitializedAsync()
     {
@@ -24,6 +30,24 @@ public class DeveloperPageBase : ComponentBase
             _presaleData = await DashboardManager.GetPresaleDataFromCurrentMonthAsync();
 
             await GetUserOperators();
+
+            if (PresaleData is null)
+            {
+                return;
+            }
+
+            var reportInProgress = ReportService.GenerateApprovalStatusReport(ApprovalStatus.InProgress, PresaleData);
+            var reportClosedLost = ReportService.GenerateApprovalStatusReport(ApprovalStatus.CloseLost, PresaleData);
+            var reportRejected = ReportService.GenerateApprovalStatusReport(ApprovalStatus.Reject, PresaleData);
+            var reportExpansion = ReportService.GenerateApprovalStatusReport(ApprovalStatus.Expansion, PresaleData);
+            var reportApproved = ReportService.GenerateApprovalStatusReport(ApprovalStatus.Approve, PresaleData);
+
+            _approvalStatusReportModels.Add(reportInProgress);
+            _approvalStatusReportModels.Add(reportClosedLost);
+            _approvalStatusReportModels.Add(reportRejected);
+            _approvalStatusReportModels.Add(reportExpansion);
+            _approvalStatusReportModels.Add(reportApproved);
+
 
             _isInitialized = true;
         }
