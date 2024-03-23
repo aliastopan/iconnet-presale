db0ee4686a2bcd7e2a06b75db7f3c6c935aa5b4a
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-23 20:58:51 +0700

    introduce `RootCauseReportModel`

---
+++ src/IConnet.Presale.WebApp/Models/Presales/RootCauseReportModel.cs
@@ -0,0 +1,37 @@
+namespace IConnet.Presale.WebApp.Models.Presales;
+
+public class RootCauseReportModel
+{
+    public RootCauseReportModel(string rootCause,
+        List<string> offices, List<int> rootCausePerOffice)
+    {
+        RootCause = rootCause;
+        RootCausePerOffice = new Dictionary<string, int>();
+
+        if (offices.Count != RootCausePerOffice.Count)
+        {
+            throw new ArgumentException();
+        }
+
+        for (int i = 0; i < offices.Count; i++)
+        {
+            RootCausePerOffice[offices[i]] = rootCausePerOffice[i];
+        }
+    }
+
+    public string RootCause { get; init; }
+    public Dictionary<string, int> RootCausePerOffice { get; init; } = default!;
+    public int GrandTotal => GetGrandTotal();
+
+    private int GetGrandTotal()
+    {
+        int grandTotal = 0;
+
+        foreach (var value in RootCausePerOffice.Values)
+        {
+            grandTotal += value;
+        }
+
+        return grandTotal;
+    }
+}
