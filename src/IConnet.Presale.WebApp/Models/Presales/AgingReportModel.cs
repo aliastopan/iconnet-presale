50f245309e9d5c127136c924f5f62608b76182a1
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-24 00:38:44 +0700

    introduce `AgingReportModel`

---
+++ src/IConnet.Presale.WebApp/Models/Presales/AgingReportModel.cs
@@ -0,0 +1,15 @@
+namespace IConnet.Presale.WebApp.Models.Presales;
+
+public class AgingReportModel
+{
+    public AgingReportModel(TimeSpan average, TimeSpan min, TimeSpan max)
+    {
+        Average = average;
+        Min = min;
+        Max = max;
+    }
+
+    public TimeSpan Average { get; init; }
+    public TimeSpan Min { get; init; }
+    public TimeSpan Max { get; init; }
+}
