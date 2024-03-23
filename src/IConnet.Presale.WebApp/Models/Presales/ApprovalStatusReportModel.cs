621ba552e7a146e3a10ab4ba13440ccdd62e187e
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-23 10:19:32 +0700

    introduce `ApprovalStatusReportModel`

---
+++ src/IConnet.Presale.WebApp/Models/Presales/ApprovalStatusReportModel.cs
@@ -0,0 +1,27 @@
+namespace IConnet.Presale.WebApp.Models.Presales;
+
+public class ApprovalStatusReportModel
+{
+    public ApprovalStatusReportModel(ApprovalStatus approvalStatus,
+        List<string> offices, List<int> statusPerOffice)
+    {
+        ApprovalStatus = approvalStatus;
+        StatusPerOffice = new Dictionary<string, int>();
+
+        // ensure number of offices and status per office are equal
+        if (offices.Count != statusPerOffice.Count)
+        {
+            throw new ArgumentException();
+        }
+
+        // populate dictionary
+        for (int i = 0; i < offices.Count; i++)
+        {
+            StatusPerOffice[offices[i]] = statusPerOffice[i];
+        }
+    }
+
+    public ApprovalStatus ApprovalStatus { get; set; }
+    public Dictionary<string, int> StatusPerOffice { get; set; } = default!;
+
+}
