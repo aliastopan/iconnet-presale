609058922dbe3860117b2674cfce5a6999b6796e
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-23 12:36:35 +0700

    feat: `ApprovalStatusReportTable` component

--- src/IConnet.Presale.WebApp/Components/Dashboards/ApprovalStatusReportTable.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Dashboards/ApprovalStatusReportTable.razor.cs
@@ -1,7 +1,9 @@
+using IConnet.Presale.WebApp.Models.Presales;
+
 namespace IConnet.Presale.WebApp.Components.Dashboards;
 
 public partial class ApprovalStatusReportTable : ComponentBase
 {
     [Parameter]
-    public List<ApprovalStatusReportModel> ApprovalStatusReportModels { get; set; }
+    public List<ApprovalStatusReportModel> Models { get; set; } = [];
 }
