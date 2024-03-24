d16d714d13b5dd0ac3dedba4930d3806075d72c4
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-24 21:59:06 +0700

    introduce `ChatCallResponsAgingReportModel`

---
+++ src/IConnet.Presale.WebApp/Models/Presales/ChatCallResponsAgingReportModel.cs
@@ -0,0 +1,50 @@
+namespace IConnet.Presale.WebApp.Models.Presales;
+
+public class ChatCallResponsAgingReportModel
+{
+    public ChatCallResponsAgingReportModel(Guid helpdeskId, string username,
+        TimeSpan average, TimeSpan min, TimeSpan max, int chatCallResponsCount)
+    {
+        HelpdeskId = helpdeskId;
+        Username = username;
+        Average = average;
+        Min = min;
+        Max = max;
+        ChatCallResponsCount = chatCallResponsCount;
+    }
+
+    public Guid HelpdeskId { get; init; }
+    public string Username { get; init; }
+    public TimeSpan Average { get; init; }
+    public TimeSpan Min { get; init; }
+    public TimeSpan Max { get; init; }
+    public int ChatCallResponsCount { get; init; }
+
+    public string GetDisplayAverageAging()
+    {
+        return ChatCallResponsCount > 0
+            ? Average.ToReadableFormat()
+            : "Tidak Pernah Validasi";
+    }
+
+    public string GetDisplayMinAging()
+    {
+        return ChatCallResponsCount > 0
+            ? Min.ToReadableFormat()
+            : "Tidak Pernah Validasi";
+    }
+
+    public string GetDisplayMaxAging()
+    {
+        return ChatCallResponsCount > 0
+            ? Max.ToReadableFormat()
+            : "Tidak Pernah Validasi";
+    }
+
+    public string GetDisplayChatCallResponsCount()
+    {
+        return ChatCallResponsCount > 0
+            ? ChatCallResponsCount.ToString()
+            : "Tidak Pernah Validasi";
+    }
+}
