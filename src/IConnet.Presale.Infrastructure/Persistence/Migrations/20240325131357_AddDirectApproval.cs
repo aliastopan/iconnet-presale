affe561d9049e0f4a93205fba562919840c4ae53
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-25 20:18:01 +0700

    migration: alter `AddDirectApproval`

---
+++ src/IConnet.Presale.Infrastructure/Persistence/Migrations/20240325131357_AddDirectApproval.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IConnet.Presale.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDirectApproval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "direct_approval",
+                table: "dbo.work_paper",
+                type: "varchar(32)",
+                maxLength: 32,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "direct_approval",
+                table: "dbo.work_paper");
+        }
+    }
+}
