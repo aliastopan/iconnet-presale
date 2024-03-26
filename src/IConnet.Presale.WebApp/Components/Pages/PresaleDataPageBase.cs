bc03ed552c78b8ecdb00ce40d540884beb42f068
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-26 12:29:28 +0700

    refactor: `FilterWorkPapers` return with `OrderBy`

--- src/IConnet.Presale.WebApp/Components/Pages/PresaleDataPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/PresaleDataPageBase.cs
@@ -65,9 +65,11 @@ public class PresaleDataPageBase : WorkloadPageBase, IPageNavigation
             return base.WorkPapers;
         }
 
-        IQueryable<WorkPaper>? workPapers = FilterComponent.FilterWorkPapers(base.WorkPapers);
+        IQueryable<WorkPaper>? workPapers = FilterComponent.FilterWorkPapers(base.WorkPapers)?
+            .OrderBy(x => x.ApprovalOpportunity.TglPermohonan);
 
         ColumnWidth.SetColumnWidth(workPapers);
+
         return workPapers;
     }
 
