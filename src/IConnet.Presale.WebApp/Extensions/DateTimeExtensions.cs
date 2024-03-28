5a7e043f253785f327951df1345c92c5536dc2dd
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-28 12:12:00 +0700

    chore: adjust readable `DateTime` format

--- src/IConnet.Presale.WebApp/Extensions/DateTimeExtensions.cs
+++ src/IConnet.Presale.WebApp/Extensions/DateTimeExtensions.cs
@@ -7,6 +7,6 @@ public static class DateTimeExtensions
     public static string ToReadableFormat(this DateTime dateTime)
     {
         var cultureInfo = new CultureInfo("id-ID");
-        return dateTime.ToString("dd MMM yyyy HH:mm", cultureInfo);
+        return dateTime.ToString("dd MMM yyyy HH:mm", cultureInfo).Replace(".", ":");;
     }
 }
