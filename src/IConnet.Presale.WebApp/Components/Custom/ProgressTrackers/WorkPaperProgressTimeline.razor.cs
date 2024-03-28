edad3a595163425caefc2a98de25c1933eb2ef80
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-28 10:17:09 +0700

    introduce `WorkPaperProgressTimeline`

---
+++ src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTimeline.razor.cs
@@ -0,0 +1,7 @@
+namespace IConnet.Presale.WebApp.Components.Custom.ProgressTrackers;
+
+public partial class WorkPaperProgressTimeline : ComponentBase
+{
+    [Parameter]
+    public WorkPaper? WorkPaper { get; set; }
+}
