ca08406fcb3571c5361445d220d97a6e72e32d69
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-28 11:50:58 +0700

    mock: color-coded timeline step

--- src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTimeline.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTimeline.razor.cs
@@ -4,4 +4,38 @@ public partial class WorkPaperProgressTimeline : ComponentBase
 {
     [Parameter]
     public WorkPaper? WorkPaper { get; set; }
+
+    public bool IsAtLeastImportUnverified => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.ImportUnverified;
+    public bool IsAtLeastReinstated => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.Reinstated;
+    public bool IsAtLeastImportVerified => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.ImportVerified;
+    public bool IsAtLeastValidating => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.Validating;
+    public bool IsAtLeastWaitingApproval => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.WaitingApproval;
+
+    public string GetImportTimelineStepCss()
+    {
+        return IsAtLeastImportUnverified
+            ? "timeline-step timeline-step-active"
+            : "timeline-step timeline-step-pending";
+    }
+
+    public string GetVerificationTimelineStepCss()
+    {
+        return IsAtLeastImportVerified
+            ? "timeline-step timeline-step-active"
+            : "timeline-step timeline-step-pending";
+    }
+
+    public string GetValidatingTimelineStepCss()
+    {
+        return IsAtLeastImportVerified
+            ? "timeline-step timeline-step-active"
+            : "timeline-step timeline-step-pending";
+    }
+
+    public string GetWaitingApprovalTimelineStepCss()
+    {
+        return IsAtLeastWaitingApproval
+            ? "timeline-step timeline-step-active"
+            : "timeline-step timeline-step-pending";
+    }
 }
