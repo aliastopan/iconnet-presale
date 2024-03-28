676ffe1416a1ce14bdef01d793871dac0b6d3448
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-28 13:44:04 +0700

    mock: timeline date display

--- src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTimeline.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Custom/ProgressTrackers/WorkPaperProgressTimeline.razor.cs
@@ -10,6 +10,7 @@ public partial class WorkPaperProgressTimeline : ComponentBase
     public bool IsAtLeastImportVerified => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.ImportVerified;
     public bool IsAtLeastValidating => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.Validating;
     public bool IsAtLeastWaitingApproval => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.WaitingApproval;
+    public bool IsAtLeastDoneProcessing => WorkPaper!.WorkPaperLevel >= WorkPaperLevel.DoneProcessing;
 
     public string GetImportTimelineStepCss()
     {
@@ -18,9 +19,48 @@ public partial class WorkPaperProgressTimeline : ComponentBase
             : "timeline-step timeline-step-pending";
     }
 
+    public string GetImportTimestamp()
+    {
+        string dateTimeString = $"{WorkPaper!.ApprovalOpportunity.SignatureImport.TglAksi.ToReadableFormat()}";
+
+        return !IsAtLeastImportUnverified
+            ? ""
+            : dateTimeString;
+    }
+
+    public string GetVerificationTimestamp()
+    {
+        bool hasSignatureVerification = !WorkPaper!.ApprovalOpportunity.SignatureVerifikasiImport.IsEmptySignature();
+        string dateTimeString = $"{WorkPaper!.ApprovalOpportunity.SignatureVerifikasiImport.TglAksi.ToReadableFormat()}";
+
+        return !(IsAtLeastValidating && hasSignatureVerification)
+            ? ""
+            : dateTimeString;
+    }
+
+    public string GetValidationTimestamp()
+    {
+        bool hasSignatureChatCallRespons = !WorkPaper!.ProsesValidasi.SignatureChatCallRespons.IsEmptySignature();
+        string dateTimeString = $"{WorkPaper!.ProsesValidasi.SignatureChatCallRespons.TglAksi.ToReadableFormat()}";
+
+        return !(IsAtLeastWaitingApproval && hasSignatureChatCallRespons)
+            ? ""
+            : dateTimeString;
+    }
+
+    public string GetApprovalTimestamp()
+    {
+        bool hasSignatureApproval = !WorkPaper!.ProsesApproval.SignatureApproval.IsEmptySignature();
+        string dateTimeString = $"{WorkPaper!.ProsesApproval.SignatureApproval.TglAksi.ToReadableFormat()}";
+
+        return !(IsAtLeastDoneProcessing && hasSignatureApproval)
+            ? ""
+            : dateTimeString;
+    }
+
     public string GetVerificationTimelineStepCss()
     {
-        return IsAtLeastImportVerified
+        return IsAtLeastReinstated
             ? "timeline-step timeline-step-active"
             : "timeline-step timeline-step-pending";
     }
