fd135296e082a5d588d6beb3af8612b2d10bf4d6
Taufan Augusta <taufan.augusta@protonmail.com>
2024-03-30 13:14:03 +0700

    migration: add `DirectApproval` table

---
+++ src/IConnet.Presale.Infrastructure/Persistence/Migrations/20240330061050_UpdateDirectApproval.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IConnet.Presale.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDirectApproval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "dbo.direct_approval",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    order = table.Column<int>(type: "int", nullable: false),
+                    description = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    is_deleted = table.Column<bool>(type: "tinyint(1)", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dbo.direct_approval", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "dbo.direct_approval");
+        }
+    }
+}
