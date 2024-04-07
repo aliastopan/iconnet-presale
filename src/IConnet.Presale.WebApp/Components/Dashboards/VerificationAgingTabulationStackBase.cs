3dca0bc7a86edb62a87219603c8bf9994ffdc48c
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-08 01:04:38 +0700

    introduce `VerificationAgingTabulationStack` component

---
+++ src/IConnet.Presale.WebApp/Components/Dashboards/VerificationAgingTabulationStackBase.cs
@@ -0,0 +1,12 @@
+using IConnet.Presale.WebApp.Models.Presales.Reports;
+
+namespace IConnet.Presale.WebApp.Components.Dashboards;
+
+public class VerificationAgingTabulationStackBase : ReportTabulationStackBase
+{
+    [Parameter] public List<VerificationAgingReportModel> UpperBoundaryModels { get; set; } = [];
+    [Parameter] public List<VerificationAgingReportModel> MiddleBoundaryModels { get; set; } = [];
+    [Parameter] public List<VerificationAgingReportModel> LowerBoundaryModels { get; set; } = [];
+
+    public bool IsPageView { get; set; }
+}
