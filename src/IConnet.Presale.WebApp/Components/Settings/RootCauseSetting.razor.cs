4ee68db467fbdaac420fad72642bc2e360a15f07
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-08 23:12:32 +0700

    introduce: `RootCauseSetting` component

---
+++ src/IConnet.Presale.WebApp/Components/Settings/RootCauseSetting.razor.cs
@@ -0,0 +1,7 @@
+namespace IConnet.Presale.WebApp.Components.Settings;
+
+public partial class RootCauseSetting
+{
+    [Parameter]
+    public IQueryable<RootCauseSettingModel>? Models { get; set; }
+}
