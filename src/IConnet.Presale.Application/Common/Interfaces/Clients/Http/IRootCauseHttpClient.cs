79a0c9fac1bdc5de6c8e07756a920fcf801c50aa
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-10 09:50:45 +0700

    add `AddRootCause` async http method

--- src/IConnet.Presale.Application/Common/Interfaces/Clients/Http/IRootCauseHttpClient.cs
+++ src/IConnet.Presale.Application/Common/Interfaces/Clients/Http/IRootCauseHttpClient.cs
@@ -3,4 +3,5 @@ namespace IConnet.Presale.Application.Common.Interfaces.Clients.Http;
 public interface IRootCauseHttpClient : IHttpClientBase
 {
     Task<HttpResult> GetRootCausesAsync();
+    Task<HttpResult> AddRootCauseAsync(int order, string cause);
 }
--- src/IConnet.Presale.Infrastructure/Clients/Http/RootCauseHttpClient.cs
+++ src/IConnet.Presale.Infrastructure/Clients/Http/RootCauseHttpClient.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using System.Text.Json;
 using IConnet.Presale.Shared.Contracts;
+using IConnet.Presale.Shared.Contracts.Common;
 
 namespace IConnet.Presale.Infrastructure.Clients.Http;
 
@@ -24,6 +27,33 @@ internal sealed class RootCauseHttpClient : HttpClientBase, IRootCauseHttpClient
         var requestUri = UriEndpoint.RootCauses.GetRootCauses;
 
         using var responseMessage = await HttpClient.GetAsync(requestUri);
+
+        return new HttpResult
+        {
+            IsSuccessStatusCode = responseMessage.IsSuccessStatusCode,
+            Headers = responseMessage.Headers,
+            Content = await responseMessage.Content.ReadAsStringAsync()
+        };
+    }
+
+    public async Task<HttpResult> AddRootCauseAsync(int order, string cause)
+    {
+        var isResponding = await IsHostRespondingAsync();
+        if (!isResponding)
+        {
+            return new HttpResult
+            {
+                IsSuccessStatusCode = false
+            };
+        }
+
+        var request = new AddRootCauseRequest(order, cause);
+
+        var jsonBody = JsonSerializer.Serialize(request);
+        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+        var requestUri = UriEndpoint.RootCauses.AddRootCause;
+
+        using var responseMessage = await HttpClient.PostAsync(requestUri, content);
 
         return new HttpResult
         {
