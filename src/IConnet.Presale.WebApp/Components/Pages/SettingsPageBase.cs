c53e3f972d31aadd053d83095c0b6316b11135f7
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-13 16:37:30 +0700

    feat: `DirectApproval` settings

--- src/IConnet.Presale.WebApp/Components/Pages/SettingsPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/SettingsPageBase.cs
@@ -3,16 +3,19 @@ namespace IConnet.Presale.WebApp.Components.Pages;
 public class SettingsPageBase : ComponentBase
 {
     [Inject] public RootCauseManager RootCauseManager { get; set; } = default!;
+    [Inject] public DirectApprovalManager DirectApprovalManager { get; set; } = default!;
 
     public bool IsInitialized { get; set; } = true;
 
     public IQueryable<RootCauseSettingModel>? RootCauseSettingModels { get; set; }
+    public IQueryable<DirectApprovalSettingModel>? DirectApprovalSettingModels { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
         IsInitialized = false;
 
         await ReloadRootCauseAsync();
+        await ReloadDirectApprovalAsync();
 
         IsInitialized = true;
     }
@@ -20,5 +23,10 @@ public class SettingsPageBase : ComponentBase
     protected async Task ReloadRootCauseAsync()
     {
         RootCauseSettingModels = await RootCauseManager.GetRootCauseSettingModelsAsync();
+    }
+
+    protected async Task ReloadDirectApprovalAsync()
+    {
+        DirectApprovalSettingModels = await DirectApprovalManager.GetDirectApprovalSettingModelsAsync();
     }
 }
