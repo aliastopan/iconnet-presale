45cc0e73575bab5a55b31092e40c727e01f0ad1c
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-13 15:25:18 +0700

    update `DirectApproval` http client

--- src/IConnet.Presale.Application/Common/Interfaces/Clients/Http/IDirectApprovalHttpClient.cs
+++ src/IConnet.Presale.Application/Common/Interfaces/Clients/Http/IDirectApprovalHttpClient.cs
@@ -3,4 +3,6 @@ namespace IConnet.Presale.Application.Common.Interfaces.Clients.Http;
 public interface IDirectApprovalHttpClient : IHttpClientBase
 {
     Task<HttpResult> GetDirectApprovalsAsync();
+    Task<HttpResult> AddDirectApprovalAsync(int order, string description);
+    Task<HttpResult> ToggleSoftDeletionAsync(Guid directApprovalId, bool isDeleted);
 }
--- src/IConnet.Presale.Infrastructure/Clients/Http/DirectApprovalHttpClient.cs
+++ src/IConnet.Presale.Infrastructure/Clients/Http/DirectApprovalHttpClient.cs
@@ -1,4 +1,7 @@
+using System.Text;
+using System.Text.Json;
 using IConnet.Presale.Shared.Contracts;
+using IConnet.Presale.Shared.Contracts.Common;
 
 namespace IConnet.Presale.Infrastructure.Clients.Http;
 
@@ -24,6 +27,60 @@ internal sealed class DirectApprovalHttpClient : HttpClientBase, IDirectApproval
         var requestUri = UriEndpoint.DirectApproval.GetDirectApprovals;
 
         using var responseMessage = await HttpClient.GetAsync(requestUri);
+
+        return new HttpResult
+        {
+            IsSuccessStatusCode = responseMessage.IsSuccessStatusCode,
+            Headers = responseMessage.Headers,
+            Content = await responseMessage.Content.ReadAsStringAsync()
+        };
+    }
+
+    public async Task<HttpResult> AddDirectApprovalAsync(int order, string description)
+    {
+        var isResponding = await IsHostRespondingAsync();
+        if (!isResponding)
+        {
+            return new HttpResult
+            {
+                IsSuccessStatusCode = false
+            };
+        }
+
+        var request = new AddDirectApprovalRequest(order, description);
+
+        var jsonBody = JsonSerializer.Serialize(request);
+        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+        var requestUri = UriEndpoint.DirectApproval.AddDirectApproval;
+
+        using var responseMessage = await HttpClient.PostAsync(requestUri, content);
+
+        return new HttpResult
+        {
+            IsSuccessStatusCode = responseMessage.IsSuccessStatusCode,
+            Headers = responseMessage.Headers,
+            Content = await responseMessage.Content.ReadAsStringAsync()
+        };
+    }
+
+    public async Task<HttpResult> ToggleSoftDeletionAsync(Guid directApprovalId, bool isDeleted)
+    {
+        var isResponding = await IsHostRespondingAsync();
+        if (!isResponding)
+        {
+            return new HttpResult
+            {
+                IsSuccessStatusCode = false
+            };
+        }
+
+        var request = new ToggleDirectApprovalSoftDeletionRequest(directApprovalId, isDeleted);
+
+        var jsonBody = JsonSerializer.Serialize(request);
+        var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+        var requestUri = UriEndpoint.RootCauses.ToggleSoftDeletion;
+
+        using var responseMessage = await HttpClient.PostAsync(requestUri, content);
 
         return new HttpResult
         {
