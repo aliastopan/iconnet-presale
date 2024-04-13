9c8615cb7122f25312237186b86501725083f0b6
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-13 14:44:33 +0700

    feat (cqrs): add `DirectApproval` and toggle soft deletion

--- src/IConnet.Presale.Api/Endpoints/Common/DirectApprovalEndpoints.cs
+++ src/IConnet.Presale.Api/Endpoints/Common/DirectApprovalEndpoints.cs
@@ -1,4 +1,7 @@
+using IConnet.Presale.Application.DirectApprovals.Commands.AddDirectApproval;
+using IConnet.Presale.Application.DirectApprovals.Commands.ToggleSoftDeletion;
 using IConnet.Presale.Application.DirectApprovals.Queries;
+using IConnet.Presale.Shared.Contracts.Common;
 
 namespace IConnet.Presale.Api.Endpoints.Common;
 
@@ -7,6 +10,8 @@ public class DirectApprovalEndpoints : IEndpointDefinition
     public void DefineEndpoints(WebApplication app)
     {
         app.MapGet(UriEndpoint.DirectApproval.GetDirectApprovals, GetDirectApprovals).AllowAnonymous();
+        app.MapPost(UriEndpoint.DirectApproval.AddDirectApproval, AddDirectApproval).AllowAnonymous();
+        app.MapPost(UriEndpoint.DirectApproval.ToggleSoftDeletion, ToggleSoftDeletion).AllowAnonymous();
     }
 
     internal async Task<IResult> GetDirectApprovals([FromServices] ISender sender,
@@ -26,5 +31,33 @@ public class DirectApprovalEndpoints : IEndpointDefinition
                 Instance = httpContext.Request.Path
             },
             context: httpContext));;
+    }
+
+    internal async Task<IResult> AddDirectApproval([FromServices] ISender sender,
+        AddDirectApprovalRequest request, HttpContext httpContext)
+    {
+        var command = new AddDirectApprovalCommand(request.Order, request.Description);
+        var result = await sender.Send(command);
+
+        return result.Match(() => Results.Ok(),
+            error => error.AsProblem(new ProblemDetails
+            {
+                Title = "Failed to add Direct Approval"
+            },
+            context: httpContext));
+    }
+
+    internal async Task<IResult> ToggleSoftDeletion([FromServices] ISender sender,
+        ToggleDirectApprovalSoftDeletionRequest request, HttpContext httpContext)
+    {
+        var command = new ToggleDirectApprovalSoftDeletionCommand(request.DirectApprovalId, request.IsDeleted);
+        var result = await sender.Send(command);
+
+        return result.Match(() => Results.Ok(),
+            error => error.AsProblem(new ProblemDetails
+            {
+                Title = "Failed to Toggle Soft Deletion"
+            },
+            context: httpContext));
     }
 }
