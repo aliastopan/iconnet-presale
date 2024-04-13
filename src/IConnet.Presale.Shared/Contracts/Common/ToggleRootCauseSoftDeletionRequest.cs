40f33a735a6dccef01c01c046f8dcdc52b8aa1a4
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-13 08:49:47 +0700

    drop unnecessary namespace

--- src/IConnet.Presale.Shared/Contracts/Common/ToggleRootCauseSoftDeletionRequest.cs
+++ src/IConnet.Presale.Shared/Contracts/Common/ToggleRootCauseSoftDeletionRequest.cs
@@ -1,5 +1,3 @@
-using IConnet.Presale.Shared.Interfaces.Models.Common;
-
 namespace IConnet.Presale.Shared.Contracts.Common;
 
 public record ToggleRootCauseSoftDeletionRequest(Guid RootCauseId, bool IsDeleted) : IToggleRootCauseSoftDeletion;
