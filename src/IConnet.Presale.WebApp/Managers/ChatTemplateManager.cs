73192a75a111cc2b44fa71320c1937b268c77d84
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-13 21:28:58 +0700

    add `GetChatTemplateSettingModels` async method

--- src/IConnet.Presale.WebApp/Managers/ChatTemplateManager.cs
+++ src/IConnet.Presale.WebApp/Managers/ChatTemplateManager.cs
@@ -9,12 +9,18 @@ public class ChatTemplateManager
     private readonly List<ChatTemplateModel> _chatTemplateModels = new List<ChatTemplateModel>();
     private readonly IChatTemplateHttpClient _chatTemplateHttpClient;
     private readonly IConfiguration _configuration;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
 
     public ChatTemplateManager(IChatTemplateHttpClient chatTemplateHttpClient,
         IConfiguration configuration)
     {
         _chatTemplateHttpClient = chatTemplateHttpClient;
         _configuration = configuration;
+
+        _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
     }
 
     public List<ChatTemplateModel> ChatTemplateModels => _chatTemplateModels;
@@ -48,6 +54,41 @@ public class ChatTemplateManager
         {
             Log.Fatal("Fatal error occurred: {message}", exception.Message);
             Environment.Exit(1);
+        }
+    }
+
+    public async Task<List<ChatTemplateSettingModel>> GetChatTemplateSettingModelsAsync()
+    {
+        List<ChatTemplateSettingModel> chatTemplateSettingModels = [];
+
+        try
+        {
+            var httpResult = await _chatTemplateHttpClient.GetAvailableChatTemplatesAsync();
+
+            if (httpResult.IsSuccessStatusCode)
+            {
+                var response = JsonSerializer.Deserialize<GetChatTemplatesQueryResponse>(httpResult.Content, _jsonSerializerOptions);
+                ICollection<ChatTemplateDto> chatTemplateDtos = response!.ChatTemplateDtos;
+
+                foreach (var dto in chatTemplateDtos)
+                {
+                    chatTemplateSettingModels.Add(new ChatTemplateSettingModel(dto));
+                }
+            }
+            else
+            {
+                var problemDetails = JsonSerializer.Deserialize<ProblemDetails>(httpResult.Content, _jsonSerializerOptions);
+                var extension = problemDetails.GetProblemDetailsExtension();
+                Log.Warning("Error {message}: ", extension.Errors.First().Message);
+            }
+
+            return chatTemplateSettingModels;
+        }
+        catch (Exception exception)
+        {
+            Log.Fatal("Fatal error occurred: {message}", exception.Message);
+
+            return chatTemplateSettingModels;
         }
     }
 
