0b05a953d2b81944a818f7200343fd15460b96eb
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-13 20:46:11 +0700

    introduce `ChatTemplateSettingModel`

---
+++ src/IConnet.Presale.WebApp/Models/Common/ChatTemplateSettingModel.cs
@@ -0,0 +1,19 @@
+using IConnet.Presale.Shared.Contracts.Common;
+
+namespace IConnet.Presale.WebApp.Models.Common;
+
+public class ChatTemplateSettingModel
+{
+    public ChatTemplateSettingModel(ChatTemplateDto chatTemplateDto)
+    {
+        ChatTemplateId = chatTemplateDto.ChatTemplateId;
+        TemplateName = chatTemplateDto.TemplateName;
+        Sequence = chatTemplateDto.Sequence;
+        Content = chatTemplateDto.Content;
+    }
+
+    public Guid ChatTemplateId { get; set; }
+    public string TemplateName { get; set; }
+    public int Sequence { get; set; }
+    public string Content { get; set; }
+}
