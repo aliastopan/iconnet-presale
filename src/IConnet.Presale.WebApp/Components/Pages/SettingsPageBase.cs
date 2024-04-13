9e916065460e8dc81a43e6ad6cef6140655c1981
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-13 22:15:55 +0700

    mock: `ChatTemplate` setting

--- src/IConnet.Presale.WebApp/Components/Pages/SettingsPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/SettingsPageBase.cs
@@ -4,11 +4,13 @@ public class SettingsPageBase : ComponentBase
 {
     [Inject] public RootCauseManager RootCauseManager { get; set; } = default!;
     [Inject] public DirectApprovalManager DirectApprovalManager { get; set; } = default!;
+    [Inject] public ChatTemplateManager ChatTemplateManager { get; set; } = default!;
 
     public bool IsInitialized { get; set; } = true;
 
     public IQueryable<RootCauseSettingModel>? RootCauseSettingModels { get; set; }
     public IQueryable<DirectApprovalSettingModel>? DirectApprovalSettingModels { get; set; }
+    public IQueryable<string>? ChatTemplateAvailable { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
@@ -16,6 +18,7 @@ public class SettingsPageBase : ComponentBase
 
         await ReloadRootCauseAsync();
         await ReloadDirectApprovalAsync();
+        await ReloadChatTemplatesAsync();
 
         IsInitialized = true;
     }
@@ -28,5 +31,19 @@ public class SettingsPageBase : ComponentBase
     protected async Task ReloadDirectApprovalAsync()
     {
         DirectApprovalSettingModels = await DirectApprovalManager.GetDirectApprovalSettingModelsAsync();
+    }
+
+    protected async Task ReloadChatTemplatesAsync()
+    {
+        List<string> chatTemplateNames;
+        List<ChatTemplateSettingModel> chatTemplates;
+
+        chatTemplates = await ChatTemplateManager.GetChatTemplateSettingModelsAsync();
+        chatTemplateNames = chatTemplates
+            .Select(x => x.TemplateName)
+            .Distinct()
+            .ToList();
+
+        ChatTemplateAvailable = chatTemplateNames.AsQueryable();
     }
 }
