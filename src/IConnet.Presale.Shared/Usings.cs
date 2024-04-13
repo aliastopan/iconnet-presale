e101d4993d71630c79044629178b80c76cf0e9f9
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-13 08:47:44 +0700

    add global usings

---
+++ src/IConnet.Presale.Shared/Usings.cs
@@ -0,0 +1,4 @@
+global using IConnet.Presale.Shared.Common;
+global using IConnet.Presale.Shared.Interfaces.Models.Common;
+global using IConnet.Presale.Shared.Interfaces.Models.Identity;
+global using IConnet.Presale.Shared.Interfaces.Models.Presales;
