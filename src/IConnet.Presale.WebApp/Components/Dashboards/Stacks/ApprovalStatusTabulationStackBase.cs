2d70dfb328e83660533c8e8757b301f3bbb48e23
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-16 13:48:06 +0700

    feat: metric tabulation

--- src/IConnet.Presale.WebApp/Components/Dashboards/Stacks/ApprovalStatusTabulationStackBase.cs
+++ src/IConnet.Presale.WebApp/Components/Dashboards/Stacks/ApprovalStatusTabulationStackBase.cs
@@ -4,7 +4,13 @@ namespace IConnet.Presale.WebApp.Components.Dashboards.Stacks;
 
 public class ApprovalStatusTabulationStackBase : ReportTabulationStackBase
 {
+    [Inject] ReportService ReportService { get; set;} = default!;
+
     [Parameter] public List<ApprovalStatusReportModel> UpperBoundaryModels { get; set; } = [];
     [Parameter] public List<ApprovalStatusReportModel> MiddleBoundaryModels { get; set; } = [];
     [Parameter] public List<ApprovalStatusReportModel> LowerBoundaryModels { get; set; } = [];
+
+    public List<ApprovalStatusMetricModel> UpperBoundaryMetrics => ReportService.ConvertToMetrics(UpperBoundaryModels);
+    public List<ApprovalStatusMetricModel> MiddleBoundaryMetrics => ReportService.ConvertToMetrics(MiddleBoundaryModels);
+    public List<ApprovalStatusMetricModel> LowerBoundaryMetrics => ReportService.ConvertToMetrics(LowerBoundaryModels);
 }
