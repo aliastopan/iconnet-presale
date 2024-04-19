ce837bc2361d1d095aceddfd6f0d9bc70e7d9e63
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-19 16:36:34 +0700

    introduce  `RootCauseTransposeHeatMapChart` component

---
+++ src/IConnet.Presale.WebApp/Components/Dashboards/Charts/RootCauseTransposeHeatMapChart.razor.cs
@@ -0,0 +1,24 @@
+using ApexCharts;
+using IConnet.Presale.WebApp.Models.Presales.Reports;
+
+namespace IConnet.Presale.WebApp.Components.Dashboards.Charts;
+
+public partial class RootCauseTransposeHeatMapChart : ComponentBase
+{
+    [Parameter]
+    public List<RootCauseReportTransposeModel> Models { get; set; } = [];
+
+    protected ApexChartOptions<RootCauseReportTransposeModel> Options { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        Options = new ApexChartOptions<RootCauseReportTransposeModel>()
+        {
+            Chart = new Chart
+            {
+                Toolbar = new Toolbar { Show = false },
+            },
+            Colors = new List<string> { "#02768f" }
+        };
+    }
+}
