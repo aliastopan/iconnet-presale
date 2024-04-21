1a3df83da9ef3d602a15690e2f715ef34df543cf
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-21 13:29:45 +0700

    fix: `SettingsPage` implements `IPageNavigation`

--- src/IConnet.Presale.WebApp/Components/Pages/SettingsPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/SettingsPageBase.cs
@@ -1,16 +1,27 @@
 namespace IConnet.Presale.WebApp.Components.Pages;
 
-public class SettingsPageBase : ComponentBase
+public class SettingsPageBase : ComponentBase, IPageNavigation
 {
     [Inject] public RootCauseManager RootCauseManager { get; set; } = default!;
     [Inject] public DirectApprovalManager DirectApprovalManager { get; set; } = default!;
     [Inject] public ChatTemplateManager ChatTemplateManager { get; set; } = default!;
+    [Inject] public TabNavigationManager TabNavigationManager { get; set; } = default!;
 
     public bool IsInitialized { get; set; } = true;
 
     public IQueryable<RootCauseSettingModel>? RootCauseSettingModels { get; set; }
     public IQueryable<DirectApprovalSettingModel>? DirectApprovalSettingModels { get; set; }
     public IQueryable<string>? ChatTemplateAvailable { get; set; }
+
+    public TabNavigationModel PageDeclaration()
+    {
+        return new TabNavigationModel("settings", PageNavName.Settings, PageRoute.Settings);
+    }
+
+    protected override void OnInitialized()
+    {
+        TabNavigationManager.SelectTab(this);
+    }
 
     protected override async Task OnInitializedAsync()
     {
