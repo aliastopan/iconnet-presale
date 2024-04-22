24343bf9ec86e1e0f58690dfb1f8299ce18a426c
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-22 18:33:40 +0700

    refactor: parallel batch operation export .xlsx

--- src/IConnet.Presale.WebApp/Services/WorksheetService.cs
+++ src/IConnet.Presale.WebApp/Services/WorksheetService.cs
@@ -25,6 +25,43 @@ public class WorksheetService
         var exportModels = ConvertToExportModels(presaleData);
         var worksheet = workbook.Worksheets.Add("Presale Data");
 
+        SetWorksheet(worksheet);
+        PopulateWorksheet(exportModels, worksheet);
+
+        workbook.SaveAs(memoryStream);
+
+        return memoryStream.ToArray();
+    }
+
+    public List<PresaleDataXlsxModel> ConvertToExportModels(IQueryable<WorkPaper>? presaleData)
+    {
+        if (presaleData == null)
+        {
+            return new List<PresaleDataXlsxModel>();
+        }
+
+        var workPapers = presaleData.ToList();
+        var exportModels = new List<PresaleDataXlsxModel>();
+        var batchSize = 100;
+
+        for (int i = 0; i < workPapers.Count; i += batchSize)
+        {
+            var batch = workPapers.Skip(i).Take(batchSize).ToList();
+            Parallel.ForEach(batch, _parallelOptions, workPaper =>
+            {
+                var model = new PresaleDataXlsxModel(workPaper);
+                lock (exportModels)
+                {
+                    exportModels.Add(model);
+                }
+            });
+        }
+
+        return exportModels.OrderBy(x => x.TglPermohonan).ToList();
+    }
+
+    private static void SetWorksheet(IXLWorksheet worksheet)
+    {
         // approval opportunity
         worksheet.Cell(1, 1).Value = "ID PERMOHONAN";
         worksheet.Cell(1, 2).Value = "TGL PERMOHONAN";
@@ -87,92 +124,72 @@ public class WorksheetService
         worksheet.Column("B").Style.DateFormat.Format = dateTimeFormat;     // tgl permohonan
         worksheet.Column("AA").Style.DateFormat.Format = dateTimeFormat;    // waktu/tgl respons
         worksheet.Column("AQ").Style.DateFormat.Format = dateOnlyFormat;    // va terbit
-
-        int row = 2;
-
-        foreach (var exportModel in exportModels)
-        {
-            worksheet.Cell(row, 1).Value = exportModel.IdPermohonan;
-            worksheet.Cell(row, 2).Value = exportModel.TglPermohonan;
-            worksheet.Cell(row, 3).Value = exportModel.StatusPermohonan;
-            worksheet.Cell(row, 4).Value = exportModel.Layanan;
-            worksheet.Cell(row, 5).Value = exportModel.SumberPermohonan;
-            worksheet.Cell(row, 6).Value = exportModel.Splitter;
-
-            worksheet.Cell(row, 7).Value = exportModel.IdPlnPelanggan;
-            worksheet.Cell(row, 8).Value = exportModel.NamaPelanggan;
-            worksheet.Cell(row, 9).Value = exportModel.NomorTeleponPelanggan;
-            worksheet.Cell(row, 10).Value = exportModel.EmailPelanggan;
-            worksheet.Cell(row, 11).Value = exportModel.AlamatPelanggan;
-            worksheet.Cell(row, 12).Value = exportModel.NikPelanggan;
-            worksheet.Cell(row, 13).Value = exportModel.NpwpPelanggan;
-            worksheet.Cell(row, 14).Value = exportModel.KeteranganPelanggan;
-
-            worksheet.Cell(row, 15).Value = exportModel.NamaAgen;
-            worksheet.Cell(row, 16).Value = exportModel.EmailAgen;
-            worksheet.Cell(row, 17).Value = exportModel.NomorTeleponAgen;
-            worksheet.Cell(row, 18).Value = exportModel.MitraAgen;
-
-            worksheet.Cell(row, 19).Value = exportModel.RegionalBagian;
-            worksheet.Cell(row, 20).Value = exportModel.KantorPerwakilan;
-            worksheet.Cell(row, 21).Value = exportModel.Provinsi;
-            worksheet.Cell(row, 22).Value = exportModel.Kabupaten;
-            worksheet.Cell(row, 23).Value = exportModel.Kecamatan;
-            worksheet.Cell(row, 24).Value = exportModel.Kelurahan;
-            worksheet.Cell(row, 25).Value = exportModel.Koordinat;
-
-            worksheet.Cell(row, 26).Value = exportModel.Shift;
-            worksheet.Cell(row, 27).Value = exportModel.GetWaktuTanggalRespons();
-            worksheet.Cell(row, 28).Value = exportModel.LinkChatHistory;
-            worksheet.Cell(row, 29).Value = exportModel.ValidasiIdPln;
-            worksheet.Cell(row, 30).Value = exportModel.ValidasiNama;
-            worksheet.Cell(row, 31).Value = exportModel.ValidasiNomorTelepon;
-            worksheet.Cell(row, 32).Value = exportModel.ValidasiEmail;
-            worksheet.Cell(row, 33).Value = exportModel.ValidasiAlamat;
-            worksheet.Cell(row, 34).Value = exportModel.ShareLoc;
-            worksheet.Cell(row, 35).Value = exportModel.KeteranganValidasi;
-
-            worksheet.Cell(row, 36).Value = exportModel.StatusApproval;
-            worksheet.Cell(row, 37).Value = exportModel.GetDirectApproval();
-            worksheet.Cell(row, 38).Value = exportModel.RootCause;
-            worksheet.Cell(row, 39).Value = exportModel.KeteranganApproval;
-            worksheet.Cell(row, 40).Value = exportModel.JarakShareLoc;
-            worksheet.Cell(row, 41).Value = exportModel.JarakICrmPlus;
-            worksheet.Cell(row, 42).Value = exportModel.SplitterGanti;
-            worksheet.Cell(row, 43).Value = exportModel.GetVaTerbit();
-
-            row++;
-        }
-
-        workbook.SaveAs(memoryStream);
-
-        return memoryStream.ToArray();
     }
 
-    public List<PresaleDataXlsxModel> ConvertToExportModels(IQueryable<WorkPaper>? presaleData)
+    private void PopulateWorksheet(List<PresaleDataXlsxModel> exportModels, IXLWorksheet worksheet)
     {
-        if (presaleData == null)
-        {
-            return new List<PresaleDataXlsxModel>();
-        }
+        int batchSize = 100;
+        int numberOfBatches = (exportModels.Count + batchSize - 1) / batchSize;
 
-        var workPapers = presaleData.OrderByDescending(x => x.ApprovalOpportunity.TglPermohonan).ToList();
-        var exportModels = new List<PresaleDataXlsxModel>();
-        var batchSize = 100;
-
-        for (int i = 0; i < workPapers.Count; i += batchSize)
+        Parallel.For(0, numberOfBatches, _parallelOptions, batchIndex =>
         {
-            var batch = workPapers.Skip(i).Take(batchSize).ToList();
-            Parallel.ForEach(batch, _parallelOptions, workPaper =>
+            int startIndex = batchIndex * batchSize;
+            int endIndex = Math.Min(startIndex + batchSize, exportModels.Count);
+
+            for (int i = startIndex; i < endIndex; i++)
             {
-                var model = new PresaleDataXlsxModel(workPaper);
-                lock (exportModels)
-                {
-                    exportModels.Add(model);
-                }
-            });
-        }
+                var exportModel = exportModels[i];
+                int row = i + 2;
 
-        return exportModels;
+                worksheet.Cell(row, 1).Value = exportModel.IdPermohonan;
+                worksheet.Cell(row, 2).Value = exportModel.TglPermohonan;
+                worksheet.Cell(row, 3).Value = exportModel.StatusPermohonan;
+                worksheet.Cell(row, 4).Value = exportModel.Layanan;
+                worksheet.Cell(row, 5).Value = exportModel.SumberPermohonan;
+                worksheet.Cell(row, 6).Value = exportModel.Splitter;
+
+                worksheet.Cell(row, 7).Value = exportModel.IdPlnPelanggan;
+                worksheet.Cell(row, 8).Value = exportModel.NamaPelanggan;
+                worksheet.Cell(row, 9).Value = exportModel.NomorTeleponPelanggan;
+                worksheet.Cell(row, 10).Value = exportModel.EmailPelanggan;
+                worksheet.Cell(row, 11).Value = exportModel.AlamatPelanggan;
+                worksheet.Cell(row, 12).Value = exportModel.NikPelanggan;
+                worksheet.Cell(row, 13).Value = exportModel.NpwpPelanggan;
+                worksheet.Cell(row, 14).Value = exportModel.KeteranganPelanggan;
+
+                worksheet.Cell(row, 15).Value = exportModel.NamaAgen;
+                worksheet.Cell(row, 16).Value = exportModel.EmailAgen;
+                worksheet.Cell(row, 17).Value = exportModel.NomorTeleponAgen;
+                worksheet.Cell(row, 18).Value = exportModel.MitraAgen;
+
+                worksheet.Cell(row, 19).Value = exportModel.RegionalBagian;
+                worksheet.Cell(row, 20).Value = exportModel.KantorPerwakilan;
+                worksheet.Cell(row, 21).Value = exportModel.Provinsi;
+                worksheet.Cell(row, 22).Value = exportModel.Kabupaten;
+                worksheet.Cell(row, 23).Value = exportModel.Kecamatan;
+                worksheet.Cell(row, 24).Value = exportModel.Kelurahan;
+                worksheet.Cell(row, 25).Value = exportModel.Koordinat;
+
+                worksheet.Cell(row, 26).Value = exportModel.Shift;
+                worksheet.Cell(row, 27).Value = exportModel.GetWaktuTanggalRespons();
+                worksheet.Cell(row, 28).Value = exportModel.LinkChatHistory;
+                worksheet.Cell(row, 29).Value = exportModel.ValidasiIdPln;
+                worksheet.Cell(row, 30).Value = exportModel.ValidasiNama;
+                worksheet.Cell(row, 31).Value = exportModel.ValidasiNomorTelepon;
+                worksheet.Cell(row, 32).Value = exportModel.ValidasiEmail;
+                worksheet.Cell(row, 33).Value = exportModel.ValidasiAlamat;
+                worksheet.Cell(row, 34).Value = exportModel.ShareLoc;
+                worksheet.Cell(row, 35).Value = exportModel.KeteranganValidasi;
+
+                worksheet.Cell(row, 36).Value = exportModel.StatusApproval;
+                worksheet.Cell(row, 37).Value = exportModel.GetDirectApproval();
+                worksheet.Cell(row, 38).Value = exportModel.RootCause;
+                worksheet.Cell(row, 39).Value = exportModel.KeteranganApproval;
+                worksheet.Cell(row, 40).Value = exportModel.JarakShareLoc;
+                worksheet.Cell(row, 41).Value = exportModel.JarakICrmPlus;
+                worksheet.Cell(row, 42).Value = exportModel.SplitterGanti;
+                worksheet.Cell(row, 43).Value = exportModel.GetVaTerbit();
+            }
+        });
     }
 }
