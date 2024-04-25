cae4bbf426f401b2784a9227ce892c5b62bacb41
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-25 09:39:21 +0700

    feat: filter `RootCause` exclusions

--- src/IConnet.Presale.WebApp/Components/Dashboards/Filters/RootCauseExclusionDialog.razor.cs
+++ src/IConnet.Presale.WebApp/Components/Dashboards/Filters/RootCauseExclusionDialog.razor.cs
@@ -8,6 +8,9 @@ public partial class RootCauseExclusionDialog : IDialogContentComponent<RootCaus
     [CascadingParameter]
     public FluentDialog Dialog { get; set; } = default!;
 
+    protected List<string> RootCauses => GetFilteredRootCauses();
+    protected string RootCauseFilter { get; set; } = string.Empty;
+
     protected async Task SaveAsync()
     {
         await Dialog.CloseAsync(Content);
@@ -28,5 +31,17 @@ public partial class RootCauseExclusionDialog : IDialogContentComponent<RootCaus
         {
             Content.Inclusion.Remove(rootCauses);
         }
+    }
+
+    protected List<string> GetFilteredRootCauses()
+    {
+        List<string> rootCauses = Content.RootCauses.ToList();
+
+        if (RootCauseFilter.IsNullOrWhiteSpace())
+        {
+            return rootCauses;
+        }
+
+        return rootCauses.Where(option => option.Contains(RootCauseFilter, StringComparison.OrdinalIgnoreCase)).ToList();
     }
 }
