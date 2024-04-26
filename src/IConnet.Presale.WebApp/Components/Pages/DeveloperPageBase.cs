ba8a4e4bddb22247ec7d9190dfc25116947a0ad6
Taufan Augusta <taufan.augusta@protonmail.com>
2024-04-26 18:53:07 +0700

    dev's page debugging

--- src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/DeveloperPageBase.cs
@@ -1,12 +1,21 @@
-using ApexCharts;
-
 namespace IConnet.Presale.WebApp.Components.Pages;
 
 public class DeveloperPageBase : ComponentBase
 {
+    [Inject] public OptionService OptionService { get; set; } = default!;
+
+    private bool _init = false;
+
     protected override void OnInitialized()
     {
-
+        if (!_init)
+        {
+            foreach (var option in OptionService.RootCauseOptionStack)
+            {
+                LogSwitch.Debug("{0}/{1}", option.rootCause, option.classification);
+            }
+            _init = true;
+        }
     }
 
     protected override async Task OnInitializedAsync()
