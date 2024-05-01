6dd5587cebbe6e11ea8157564c9a098a2856124a
Taufan Augusta <taufan.augusta@protonmail.com>
2024-05-01 22:29:38 +0700

    introduce `CreateRootCauseDialog` component

---
+++ src/IConnet.Presale.WebApp/Components/Settings/RootCauses/CreateRootCauseDialog.razor.cs
@@ -0,0 +1,36 @@
+namespace IConnet.Presale.WebApp.Components.Settings.RootCauses;
+
+public partial class CreateRootCauseDialog : IDialogContentComponent<NewRootCauseModel>
+{
+    [Inject] public AppSettingsService AppSettingsService { get; set; } = default!;
+
+    [Parameter]
+    public NewRootCauseModel Content { get; set; } = default!;
+
+    [CascadingParameter]
+    public FluentDialog Dialog { get; set; } = default!;
+
+    protected List<string> RootCauseClassifications => AppSettingsService.RootCauseClassifications;
+    protected string NewRootCause { get; set; } = default!;
+    protected string NewRootCauseClassification { get; set; } = default!;
+
+    protected async Task SaveAsync()
+    {
+        await Dialog.CloseAsync(new NewRootCauseModel(NewRootCause, NewRootCauseClassification));
+    }
+
+    protected async Task CancelAsync()
+    {
+        await Dialog.CancelAsync();
+    }
+
+    protected void OnNewRootCauseChanged(string newRootCause)
+    {
+        NewRootCause = newRootCause.SanitizeOnlyAlphanumericAndSpaces();
+    }
+
+    protected void OnNewRootCauseClassificationChanged(string classification)
+    {
+        NewRootCauseClassification = classification;
+    }
+}
