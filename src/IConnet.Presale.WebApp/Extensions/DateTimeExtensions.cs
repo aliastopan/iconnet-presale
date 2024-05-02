e7e2aa2666d7ba9dab9e43b12908d4926b0501f7
Taufan Augusta <taufan.augusta@protonmail.com>
2024-05-02 08:41:54 +0700

    add `ToDateOnlyFormat` extension method

--- src/IConnet.Presale.WebApp/Extensions/DateTimeExtensions.cs
+++ src/IConnet.Presale.WebApp/Extensions/DateTimeExtensions.cs
@@ -7,6 +7,12 @@ public static class DateTimeExtensions
     public static string ToReadableFormat(this DateTime dateTime)
     {
         var cultureInfo = new CultureInfo("id-ID");
-        return dateTime.ToString("dd MMM yyyy HH:mm", cultureInfo).Replace(".", ":");;
+        return dateTime.ToString("dd MMM yyyy HH:mm", cultureInfo).Replace(".", ":");
+    }
+
+    public static string ToDateOnlyFormat(this DateTime dateTime)
+    {
+        var cultureInfo = new CultureInfo("id-ID");
+        return dateTime.ToString("dd/MM/yyyy", cultureInfo);
     }
 }
