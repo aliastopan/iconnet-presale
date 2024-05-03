0b02b811b2399a1277615c52be84796f3634ca2b
Taufan Augusta <taufan.augusta@protonmail.com>
2024-05-03 11:01:38 +0700

    feat: add `CustomErrorBoundary` to `GuestPage`

--- src/IConnet.Presale.WebApp/Components/Pages/GuestPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/GuestPageBase.cs
@@ -1,6 +1,17 @@
+using IConnet.Presale.WebApp.Components.Custom;
+
 namespace IConnet.Presale.WebApp.Components.Pages;
 
 public class GuestPageBase : StatusTrackingPageBase
 {
+    public CustomErrorBoundary? ErrorBoundary { get; set; }
 
+    protected override void OnInitialized()
+    {
+        if (ErrorBoundary?.CurrentException is not null)
+        {
+            Log.Information("Recovering from {exception}", ErrorBoundary.CurrentException.GetType().Name);
+            ErrorBoundary?.Recover();
+        }
+    }
 }
