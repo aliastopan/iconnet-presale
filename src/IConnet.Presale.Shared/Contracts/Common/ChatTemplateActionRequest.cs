b8502c229a671f6fd84b4107107fb7b122684bf7
Taufan Augusta <taufan.augusta@protonmail.com>
2024-05-06 10:12:12 +0700

    introduce `ChatTemplateActionCommand`

---
+++ src/IConnet.Presale.Shared/Contracts/Common/ChatTemplateActionRequest.cs
@@ -0,0 +1,4 @@
+namespace IConnet.Presale.Shared.Contracts.Common;
+
+public record ChatTemplateActionRequest(Guid ChatTemplateId, string TemplateName, int Sequence, string Content, int Action)
+    : IChatTemplateActionModel;
