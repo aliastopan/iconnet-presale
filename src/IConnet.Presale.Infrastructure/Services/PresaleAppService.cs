bb9c9810bdd02ec7a7ff9f5b15c1c3a8d9383a95
Taufan Augusta <taufan.augusta@protonmail.com>
2024-05-06 08:16:17 +0700

    update `SetDefaultSetting` options

--- src/IConnet.Presale.Infrastructure/Services/PresaleAppService.cs
+++ src/IConnet.Presale.Infrastructure/Services/PresaleAppService.cs
@@ -55,6 +55,7 @@ internal class PresaleAppService : IPresaleAppService
             },
             RootCauseClassification =
             [
+                "TIDAK ADA KLASIFIKASI",
                 "USER INVALID",
                 "KENDALA PENGECEKAN",
                 "DOUBLE INPUTAN",
