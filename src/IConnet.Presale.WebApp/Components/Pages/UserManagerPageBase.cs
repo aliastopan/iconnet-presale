270f7ae9b485df7d8373a8eaa7b31ca72bdaeff2
Taufan Augusta <taufan.augusta@protonmail.com>
2024-05-07 17:41:23 +0700

    refactor: `UserManagerPage` structure

--- src/IConnet.Presale.WebApp/Components/Pages/UserManagerPageBase.cs
+++ src/IConnet.Presale.WebApp/Components/Pages/UserManagerPageBase.cs
@@ -1,6 +1,28 @@
 namespace IConnet.Presale.WebApp.Components.Pages;
 
-public class UserManagerPageBase : ComponentBase
+public class UserManagerPageBase : ComponentBase, IPageNavigation
 {
+    [Inject] public TabNavigationManager TabNavigationManager { get; set; } = default!;
+
+    public bool IsInitialized { get; set; } = true;
+
+    public TabNavigationModel PageDeclaration()
+    {
+        return new TabNavigationModel("user-manager", PageNavName.UserManager, PageRoute.UserManager);
+    }
+
+    protected override void OnInitialized()
+    {
+        TabNavigationManager.SelectTab(this);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        IsInitialized = false;
+
+        await Task.CompletedTask;
+
+        IsInitialized = true;
+    }
 
 }
