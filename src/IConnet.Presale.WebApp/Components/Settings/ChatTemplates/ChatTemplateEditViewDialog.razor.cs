8a7da34e6727074db289fa50c4154476517c8785
Taufan Augusta <taufan.augusta@protonmail.com>
2024-05-18 21:56:20 +0700

    introduce `ChatTemplateEditViewDialog` component

---
+++ src/IConnet.Presale.WebApp/Components/Settings/ChatTemplates/ChatTemplateEditViewDialog.razor.cs
@@ -0,0 +1,52 @@
+namespace IConnet.Presale.WebApp.Components.Settings.ChatTemplates;
+
+public partial class ChatTemplateEditViewDialog : IDialogContentComponent<List<ChatTemplateSettingModel>>
+{
+    [Inject] AppSettingsService AppSettingsService { get; set; } = default!;
+
+    [Parameter]
+    public List<ChatTemplateSettingModel> Content { get; set; } = default!;
+
+    [CascadingParameter]
+    public FluentDialog Dialog { get; set; } = default!;
+
+    protected async Task SaveAsync()
+    {
+        await Dialog.CloseAsync(Content);
+    }
+
+    protected async Task CancelAsync()
+    {
+        await Dialog.CancelAsync();
+    }
+
+    public Guid ChatTemplateId { get; set; } = default!;
+    public ChatTemplateSettingModel ActiveModel = default!;
+    public string ContentEditHolder { get; set; } = default!;
+
+    protected void OnContentEditHolderChanged(string content)
+    {
+        if (ActiveModel is null)
+        {
+            return;
+        }
+
+        ActiveModel.Content = content;
+        ActiveModel.ActionSetting = ChatTemplateAction.ChatEdit;
+
+        ContentEditHolder = content;
+    }
+
+    protected void EditChat(ChatTemplateSettingModel model)
+    {
+        ActiveModel = model;
+        ContentEditHolder = model.Content;
+
+        LogSwitch.Debug("Editing Model...");
+    }
+
+    protected MarkupString GetHtmlString(string chat)
+    {
+        return (MarkupString) chat.FormatHtmlBreak();
+    }
+}
