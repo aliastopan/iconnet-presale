89a8b496085e4baabd927baf50228db822623caf
Taufan Augusta <taufan.augusta@protonmail.com>
2024-05-19 16:57:18 +0700

    add `Copy` self method

--- src/IConnet.Presale.WebApp/Models/Common/ChatTemplateSettingModel.cs
+++ src/IConnet.Presale.WebApp/Models/Common/ChatTemplateSettingModel.cs
@@ -13,9 +13,37 @@ public class ChatTemplateSettingModel
         ActionSetting = ChatTemplateAction.Default;
     }
 
+    public ChatTemplateSettingModel(string templateName, int sequence, string content, ChatTemplateAction action)
+    {
+        ChatTemplateId = Guid.NewGuid();
+        TemplateName = templateName;
+        Sequence = sequence;
+        Content = content;
+        ActionSetting = action;
+    }
+
+    public ChatTemplateSettingModel(Guid chatTemplateId, string templateName, int sequence, string content, ChatTemplateAction action)
+    {
+        ChatTemplateId = chatTemplateId;
+        TemplateName = templateName;
+        Sequence = sequence;
+        Content = content;
+        ActionSetting = action;
+    }
+
     public Guid ChatTemplateId { get; set; }
     public string TemplateName { get; set; }
     public int Sequence { get; set; }
     public string Content { get; set; }
     public ChatTemplateAction ActionSetting { get; set; }
+
+    public ChatTemplateSettingModel Copy()
+    {
+        return new ChatTemplateSettingModel(
+            this.ChatTemplateId,
+            this.TemplateName,
+            this.Sequence,
+            this.Content,
+            this.ActionSetting);
+    }
 }
